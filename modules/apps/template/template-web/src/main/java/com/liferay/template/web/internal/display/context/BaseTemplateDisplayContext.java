e388f926e08189d5e4b348345bdfd58e77c66aed
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-07-28 10:59:07 +0200

    LPS-135490 Implement creation menu logic for widget templates tab

--- modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/display/context/BaseTemplateDisplayContext.java
+++ modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/display/context/BaseTemplateDisplayContext.java
@@ -70,7 +70,7 @@ public abstract class BaseTemplateDisplayContext
 
 		_ddmWebConfiguration = ddmWebConfiguration;
 		this.liferayPortletRequest = liferayPortletRequest;
-		_liferayPortletResponse = liferayPortletResponse;
+		this.liferayPortletResponse = liferayPortletResponse;
 
 		_httpServletRequest = PortalUtil.getHttpServletRequest(
 			liferayPortletRequest);
@@ -99,7 +99,7 @@ public abstract class BaseTemplateDisplayContext
 			ddmTemplateActionDropdownItemsProvider =
 				new DDMTemplateActionDropdownItemsProvider(
 					isAddDDMTemplateEnabled(), ddmTemplate, _httpServletRequest,
-					_liferayPortletResponse);
+					liferayPortletResponse);
 
 		return ddmTemplateActionDropdownItemsProvider.getActionDropdownItems();
 	}
@@ -115,7 +115,7 @@ public abstract class BaseTemplateDisplayContext
 		}
 
 		return PortletURLBuilder.createRenderURL(
-			_liferayPortletResponse
+			liferayPortletResponse
 		).setMVCPath(
 			"/edit_ddm_template.jsp"
 		).setRedirect(
@@ -150,7 +150,7 @@ public abstract class BaseTemplateDisplayContext
 				navigationItem.setActive(
 					Objects.equals(_getTabs1(), "information-templates"));
 				navigationItem.setHref(
-					_liferayPortletResponse.createRenderURL(), "tabs1",
+					liferayPortletResponse.createRenderURL(), "tabs1",
 					"information-templates");
 				navigationItem.setLabel(
 					LanguageUtil.get(
@@ -161,7 +161,7 @@ public abstract class BaseTemplateDisplayContext
 				navigationItem.setActive(
 					Objects.equals(_getTabs1(), "widget-templates"));
 				navigationItem.setHref(
-					_liferayPortletResponse.createRenderURL(), "tabs1",
+					liferayPortletResponse.createRenderURL(), "tabs1",
 					"widget-templates");
 				navigationItem.setLabel(
 					LanguageUtil.get(_httpServletRequest, "widget-templates"));
@@ -188,7 +188,7 @@ public abstract class BaseTemplateDisplayContext
 			_getTemplateOrderByComparator());
 		ddmTemplateSearchContainer.setOrderByType(_getOrderByType());
 		ddmTemplateSearchContainer.setRowChecker(
-			new EmptyOnClickRowChecker(_liferayPortletResponse));
+			new EmptyOnClickRowChecker(liferayPortletResponse));
 
 		ddmTemplateSearchContainer.setResults(
 			DDMTemplateServiceUtil.search(
@@ -255,6 +255,7 @@ public abstract class BaseTemplateDisplayContext
 	}
 
 	protected final LiferayPortletRequest liferayPortletRequest;
+	protected final LiferayPortletResponse liferayPortletResponse;
 	protected final ThemeDisplay themeDisplay;
 
 	private String _getKeywords() {
@@ -293,7 +294,7 @@ public abstract class BaseTemplateDisplayContext
 
 	private PortletURL _getPortletURL() {
 		return PortletURLBuilder.createRenderURL(
-			_liferayPortletResponse
+			liferayPortletResponse
 		).setTabs1(
 			_getTabs1()
 		).buildPortletURL();
@@ -336,7 +337,6 @@ public abstract class BaseTemplateDisplayContext
 	private final DDMWebConfiguration _ddmWebConfiguration;
 	private final HttpServletRequest _httpServletRequest;
 	private String _keywords;
-	private final LiferayPortletResponse _liferayPortletResponse;
 	private String _orderByCol;
 	private String _orderByType;
 	private String _tabs1;
