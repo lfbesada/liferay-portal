da8d8af332ee65d5b017fbe06e0456aedc4658df
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-07-29 18:15:40 +0200

    LPS-136361 The template handler for information templates cannot be the template handler registered by their classNameId because some portlet display templates are already registered handlers with those IDs.
    Some examples:
    - AssetCategory -> AssetCategoriesNavigationPortletDisplayTemplateHandler
    - BlogsEntry -> BlogsPortletDisplayTemplateHandler
    
    Since we cannot use the classNameId template to find a suitable handler for this template, we will use resourceClassNameId and create a simple template handler for all of them.

--- modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/display/context/InformationTemplatesTemplateDisplayContext.java
+++ modules/apps/template/template-web/src/main/java/com/liferay/template/web/internal/display/context/InformationTemplatesTemplateDisplayContext.java
@@ -145,6 +145,11 @@ public class InformationTemplatesTemplateDisplayContext
 	}
 
 	@Override
+	protected long getTemplateHandlerClassNameId() {
+		return getResourceClassNameId();
+	}
+
+	@Override
 	protected String[] getTemplateLanguageTypes() {
 		return new String[] {TemplateConstants.LANG_TYPE_FTL};
 	}
