72e9be8bd892da5f7567715078789a8cc9f9bd0d
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-09-12 13:22:56 +0200

    LPS-135156 Implements finder method

--- modules/apps/site-navigation/site-navigation-service/src/main/java/com/liferay/site/navigation/service/persistence/impl/SiteNavigationMenuItemFinderImpl.java
+++ modules/apps/site-navigation/site-navigation-service/src/main/java/com/liferay/site/navigation/service/persistence/impl/SiteNavigationMenuItemFinderImpl.java
@@ -14,18 +14,66 @@
 
 package com.liferay.site.navigation.service.persistence.impl;
 
-import java.util.Collections;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.dao.orm.custom.sql.CustomSQL;
+import com.liferay.portal.kernel.dao.orm.QueryPos;
+import com.liferay.portal.kernel.dao.orm.SQLQuery;
+import com.liferay.portal.kernel.dao.orm.Session;
+import com.liferay.portal.kernel.dao.orm.Type;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.site.navigation.service.persistence.SiteNavigationMenuItemFinder;
+
 import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Lourdes Fernández Besada
  */
-public class SiteNavigationMenuItemFinderImpl {
+@Component(service = SiteNavigationMenuItemFinder.class)
+public class SiteNavigationMenuItemFinderImpl
+	extends SiteNavigationMenuItemFinderBaseImpl
+	implements SiteNavigationMenuItemFinder {
 
+	public static final String FIND_PARENTID_BY_M_TS =
+		SiteNavigationMenuItemFinder.class.getName() + ".findParentIdByM_TS";
+
+	@Override
 	public List<Long> findParentIdByM_TS(
 		long siteNavigationMenuId, String typeSettings) {
 
-		return Collections.emptyList();
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = _customSQL.get(getClass(), FIND_PARENTID_BY_M_TS);
+
+			SQLQuery sqlQuery = session.createSynchronizedSQLQuery(sql);
+
+			sqlQuery.setCacheable(true);
+			sqlQuery.addScalar("parentSiteNavigationMenuItemId", Type.LONG);
+
+			QueryPos queryPos = QueryPos.getInstance(sqlQuery);
+
+			queryPos.add(siteNavigationMenuId);
+			queryPos.add(
+				StringBundler.concat(
+					StringPool.PERCENT, typeSettings, StringPool.PERCENT));
+
+			return (List<Long>)sqlQuery.list();
+		}
+		catch (Exception exception) {
+			throw new SystemException(exception);
+		}
+		finally {
+			closeSession(session);
+		}
 	}
+
+	@Reference
+	private CustomSQL _customSQL;
 
 }
