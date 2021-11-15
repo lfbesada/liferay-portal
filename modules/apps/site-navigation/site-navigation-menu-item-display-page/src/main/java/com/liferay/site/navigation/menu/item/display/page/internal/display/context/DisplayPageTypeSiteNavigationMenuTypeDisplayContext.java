441917e1771e04d675e6eec030a3e7f993feb863
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-11-15 14:14:57 +0100

    LPS-141351 Adjusting the jsp to use the new display context and to restrict the class name to the display page type.

--- modules/apps/site-navigation/site-navigation-menu-item-display-page/src/main/java/com/liferay/site/navigation/menu/item/display/page/internal/display/context/DisplayPageTypeSiteNavigationMenuTypeDisplayContext.java
+++ modules/apps/site-navigation/site-navigation-menu-item-display-page/src/main/java/com/liferay/site/navigation/menu/item/display/page/internal/display/context/DisplayPageTypeSiteNavigationMenuTypeDisplayContext.java
@@ -50,6 +50,10 @@ public class DisplayPageTypeSiteNavigationMenuTypeDisplayContext {
 			WebKeys.THEME_DISPLAY);
 	}
 
+	public String getClassName() {
+		return _displayPageTypeContext.getClassName();
+	}
+
 	public long getClassNameId() {
 		if (_classNameId != null) {
 			return _classNameId;
