2516747d3de756218bf5d0a28c5d09a5f0478c1d
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-11-16 08:45:07 +0100

    LPS-142508 Adds multi selection feature to site navigation menu item type, default false.

--- modules/apps/site-navigation/site-navigation-admin-web/src/main/java/com/liferay/site/navigation/admin/web/internal/display/context/SiteNavigationAdminDisplayContext.java
+++ modules/apps/site-navigation/site-navigation-admin-web/src/main/java/com/liferay/site/navigation/admin/web/internal/display/context/SiteNavigationAdminDisplayContext.java
@@ -422,6 +422,8 @@ public class SiteNavigationAdminDisplayContext {
 			).put(
 				"itemSelector", siteNavigationMenuItemType.isItemSelector()
 			).put(
+				"multiSelection", siteNavigationMenuItemType.isMultiSelection()
+			).put(
 				"siteNavigationMenuId", getSiteNavigationMenuId()
 			).build());
 		dropdownItem.setLabel(
