0eadb098b481a501a62af0ada917f362f597d0bb
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-11-22 08:40:54 +0100

    LPS-142508 Adds specific action command to handle asset category multiselection

---
+++ modules/apps/site-navigation/site-navigation-menu-item-display-page/src/main/java/com/liferay/site/navigation/menu/item/display/page/internal/portlet/action/AddMultipleAssetCategorySiteNavigationMenuItemsMVCActionCommand.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.navigation.menu.item.display.page.internal.portlet.action;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.site.navigation.admin.constants.SiteNavigationAdminPortletKeys;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + SiteNavigationAdminPortletKeys.SITE_NAVIGATION_ADMIN,
+		"mvc.command.name=/navigation_menu/add_multiple_asset_category_site_navigation_menu_item"
+	},
+	service = MVCActionCommand.class
+)
+public class AddMultipleAssetCategorySiteNavigationMenuItemsMVCActionCommand
+	extends AddMultipleDisplayPageTypeSiteNavigationMenuItemMVCActionCommand {
+}
