fad0b1b7fc8666cdf3ae4178748b328a88f56bc7
Yang Cao <yang.cao@liferay.com>
2021-12-30 17:19:26 +0800

    LPS-141754 Add test to assert file entry info item field reader

--- modules/apps/document-library/document-library-web-test/src/testIntegration/java/com/liferay/document/library/web/internal/info/item/provider/test/FileEntryInfoItemFieldValuesProviderTest.java
+++ modules/apps/document-library/document-library-web-test/src/testIntegration/java/com/liferay/document/library/web/internal/info/item/provider/test/FileEntryInfoItemFieldValuesProviderTest.java
@@ -17,9 +17,13 @@ package com.liferay.document.library.web.internal.info.item.provider.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.document.library.kernel.service.DLAppService;
+import com.liferay.info.field.InfoField;
 import com.liferay.info.field.InfoFieldValue;
+import com.liferay.info.field.type.TextInfoFieldType;
 import com.liferay.info.item.InfoItemFieldValues;
+import com.liferay.info.item.field.reader.InfoItemFieldReader;
 import com.liferay.info.item.provider.InfoItemFieldValuesProvider;
+import com.liferay.info.localized.InfoLocalizedValue;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
@@ -32,6 +36,9 @@ import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceRegistration;
 
 import jodd.net.MimeTypes;
 
@@ -58,6 +65,28 @@ public class FileEntryInfoItemFieldValuesProviderTest {
 	@Before
 	public void setUp() throws Exception {
 		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test
+	public void testFileEntryInfoItemFieldReader() throws PortalException {
+		_registerFileEntryInfoItemFieldReader();
+
+		FileEntry fileEntry = _dlAppService.addFileEntry(
+			null, _group.getGroupId(),
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			RandomTestUtil.randomString(),
+			MimeTypes.MIME_APPLICATION_OCTET_STREAM,
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+			StringPool.BLANK, (byte[])null, null, null,
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
+
+		InfoItemFieldValues infoItemFieldValues =
+			_infoItemFieldValuesProvider.getInfoItemFieldValues(fileEntry);
+
+		InfoFieldValue<Object> testInfoFieldValue =
+			infoItemFieldValues.getInfoFieldValue("Test");
+
+		Assert.assertEquals("Test", testInfoFieldValue.getValue());
 	}
 
 	@Test
@@ -93,6 +122,18 @@ public class FileEntryInfoItemFieldValuesProviderTest {
 			fileEntry.getModifiedDate(), publishDateInfoFieldValue.getValue());
 	}
 
+	private void _registerFileEntryInfoItemFieldReader() {
+		TestFileEntryInfoItemFieldReader fileEntryInfoItemFieldReader =
+			new TestFileEntryInfoItemFieldReader();
+
+		Registry registry = RegistryUtil.getRegistry();
+
+		_serviceRegistration = registry.registerService(
+			(Class<InfoItemFieldReader<FileEntry>>)
+				(Class<?>)InfoItemFieldReader.class,
+			fileEntryInfoItemFieldReader);
+	}
+
 	@Inject
 	private DLAppService _dlAppService;
 
@@ -101,5 +142,35 @@ public class FileEntryInfoItemFieldValuesProviderTest {
 
 	@Inject(filter = "component.name=*.FileEntryInfoItemFieldValuesProvider")
 	private InfoItemFieldValuesProvider _infoItemFieldValuesProvider;
+
+	private ServiceRegistration<InfoItemFieldReader<FileEntry>>
+		_serviceRegistration;
+
+	private static class TestFileEntryInfoItemFieldReader
+		implements InfoItemFieldReader<FileEntry> {
+
+		@Override
+		public InfoField getField() {
+			return null;
+		}
+
+		@Override
+		public InfoField getInfoField() {
+			return InfoField.builder(
+			).infoFieldType(
+				TextInfoFieldType.INSTANCE
+			).name(
+				"Test"
+			).labelInfoLocalizedValue(
+				InfoLocalizedValue.localize(getClass(), "Test")
+			).build();
+		}
+
+		@Override
+		public Object getValue(FileEntry model) {
+			return "Test";
+		}
+
+	}
 
 }
