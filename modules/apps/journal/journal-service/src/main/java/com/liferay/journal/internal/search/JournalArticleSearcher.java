1a485f53bf200c3ec269fa0a953f3ea867367fce
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-02-02 17:02:51 +0100

    LPS-139522 Copy from JournalArticleIndexer, no logic changes

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/search/JournalArticleSearcher.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/search/JournalArticleSearcher.java
@@ -16,6 +16,7 @@ package com.liferay.journal.internal.search;
 
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.portal.kernel.search.BaseSearcher;
+import com.liferay.portal.kernel.search.Field;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -30,6 +31,19 @@ import org.osgi.service.component.annotations.Component;
 public class JournalArticleSearcher extends BaseSearcher {
 
 	public static final String CLASS_NAME = JournalArticle.class.getName();
+
+	public JournalArticleSearcher() {
+		setDefaultSelectedFieldNames(
+			Field.ASSET_TAG_NAMES, Field.ARTICLE_ID, Field.COMPANY_ID,
+			Field.DEFAULT_LANGUAGE_ID, Field.ENTRY_CLASS_NAME,
+			Field.ENTRY_CLASS_PK, Field.GROUP_ID, Field.MODIFIED_DATE,
+			Field.SCOPE_GROUP_ID, Field.VERSION, Field.UID);
+		setDefaultSelectedLocalizedFieldNames(
+			Field.CONTENT, Field.DESCRIPTION, Field.TITLE);
+		setFilterSearch(true);
+		setPermissionAware(true);
+		setSelectAllLocales(true);
+	}
 
 	@Override
 	public String getClassName() {
