8c5a94d18b306f805d4f0e15d9fa4710a4226b4d
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-06-10 10:54:08 +0200

    LPS-151745 Add LIFERAY_SHARED_ prefix to attribute name in order to propagate the check access allowed to the portlet until the PortletFragmentEntryProcessor

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-portlet/src/main/java/com/liferay/fragment/entry/processor/portlet/PortletFragmentEntryProcessor.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-portlet/src/main/java/com/liferay/fragment/entry/processor/portlet/PortletFragmentEntryProcessor.java
@@ -57,6 +57,7 @@ import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portlet.PortletPreferencesImpl;
 import com.liferay.portlet.exportimport.staging.StagingAdvicesThreadLocal;
 
@@ -469,6 +470,7 @@ public class PortletFragmentEntryProcessor implements FragmentEntryProcessor {
 
 		if (fragmentEntryLink != null) {
 			String checkAccessAllowedToPortletCacheKey = StringBundler.concat(
+				WebKeys.LIFERAY_SHARED,
 				DefaultLayoutTypeAccessPolicyImpl.class.getName(), "#",
 				ParamUtil.getLong(
 					fragmentEntryProcessorContext.getHttpServletRequest(),
