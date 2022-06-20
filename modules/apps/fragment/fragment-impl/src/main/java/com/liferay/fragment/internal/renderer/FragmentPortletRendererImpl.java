7f19e930b2c9b201f34bb2012891a6a5d5fb1c76
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-06-20 11:44:30 +0200

    LPS-156367 Master's widgets preferences shouldn't be persists using the render layout plid

--- modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/renderer/FragmentPortletRendererImpl.java
+++ modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/renderer/FragmentPortletRendererImpl.java
@@ -14,17 +14,13 @@
 
 package com.liferay.fragment.internal.renderer;
 
-import com.liferay.fragment.constants.FragmentWebKeys;
 import com.liferay.fragment.exception.FragmentEntryContentException;
-import com.liferay.fragment.model.FragmentEntryLink;
 import com.liferay.fragment.renderer.FragmentPortletRenderer;
 import com.liferay.petra.io.unsync.UnsyncStringWriter;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.constants.PortletPreferencesFactoryConstants;
 import com.liferay.portal.kernel.servlet.PipingServletResponse;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.taglib.portletext.RuntimeTag;
 
 import javax.servlet.http.HttpServletRequest;
@@ -50,29 +46,13 @@ public class FragmentPortletRendererImpl implements FragmentPortletRenderer {
 		PipingServletResponse pipingServletResponse = new PipingServletResponse(
 			httpServletResponse, unsyncStringWriter);
 
-		boolean inheritedFromMaster = false;
-
-		FragmentEntryLink fragmentEntryLink =
-			(FragmentEntryLink)httpServletRequest.getAttribute(
-				FragmentWebKeys.FRAGMENT_ENTRY_LINK);
-
-		ThemeDisplay themeDisplay =
-			(ThemeDisplay)httpServletRequest.getAttribute(
-				WebKeys.THEME_DISPLAY);
-
-		if ((fragmentEntryLink != null) && (themeDisplay != null) &&
-			(fragmentEntryLink.getPlid() != themeDisplay.getPlid())) {
-
-			inheritedFromMaster = true;
-		}
-
 		try {
 			RuntimeTag.doTag(
 				portletName, instanceId, StringPool.BLANK,
 				PortletPreferencesFactoryConstants.
 					SETTINGS_SCOPE_PORTLET_INSTANCE,
-				defaultPreferences, inheritedFromMaster, null,
-				httpServletRequest, pipingServletResponse);
+				defaultPreferences, false, null, httpServletRequest,
+				pipingServletResponse);
 		}
 		catch (Exception exception) {
 			throw new FragmentEntryContentException(exception);
