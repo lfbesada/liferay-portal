53e6ac8ab7e3ee7697c34e067e5eef88cb4052c8
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-06-21 14:58:51 +0200

    LPS-156404 SF, no need var, return directly

--- modules/apps/layout/layout-utility-page/layout-utility-page-status/src/main/java/com/liferay/layout/utility/page/status/internal/display/context/StatusDisplayContext.java
+++ modules/apps/layout/layout-utility-page/layout-utility-page-status/src/main/java/com/liferay/layout/utility/page/status/internal/display/context/StatusDisplayContext.java
@@ -49,13 +49,11 @@ public class StatusDisplayContext {
 		String exception = ParamUtil.getString(
 			_httpServletRequest, "exception");
 
-		boolean noSuchResourceException = false;
-
 		for (String key : SessionErrors.keySet(_httpServletRequest)) {
 			key = key.substring(key.lastIndexOf(StringPool.PERIOD) + 1);
 
 			if (key.startsWith("NoSuch") && key.endsWith("Exception")) {
-				noSuchResourceException = true;
+				return true;
 			}
 		}
 
@@ -63,7 +61,7 @@ public class StatusDisplayContext {
 				_httpServletRequest.getAttribute(
 					NoSuchLayoutException.class.getName()))) {
 
-			noSuchResourceException = true;
+			return true;
 		}
 		else if (Validator.isNotNull(exception)) {
 			exception = exception.substring(
@@ -72,11 +70,11 @@ public class StatusDisplayContext {
 			if (exception.startsWith("NoSuch") &&
 				exception.endsWith("Exception")) {
 
-				noSuchResourceException = true;
+				return true;
 			}
 		}
 
-		return noSuchResourceException;
+		return false;
 	}
 
 	private final HttpServletRequest _httpServletRequest;
