b28028fe62b937a1895d56102b940fdcd7da30ae
Sandro Chinea <sandrochinea@gmail.com>
2022-06-24 13:47:49 +0100

    LPS-156735 Allow selecting from Documents & Media when it's enabled

--- modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-inputs/src/main/resources/com/liferay/fragment/collection/contributor/inputs/dependencies/file-upload/index.js
+++ modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-inputs/src/main/resources/com/liferay/fragment/collection/contributor/inputs/dependencies/file-upload/index.js
@@ -12,19 +12,23 @@
  * details.
  */
 
+const MOCK_URL =
+	'http://localhost:8080/group/guest/~/control_panel/manage/-/select/infoitem/_com_liferay_site_navigation_admin_web_portlet_SiteNavigationAdminPortlet_selectInfoItem?_com_liferay_item_selector_web_portlet_ItemSelectorPortlet_0_json=%7B%22desiredItemSelectorReturnTypes%22%3A%22com.liferay.item.selector.criteria.InfoItemItemSelectorReturnType%22%2C%22itemSubtype%22%3Anull%2C%22itemType%22%3A%22com.liferay.portal.kernel.repository.model.FileEntry%22%2C%22mimeTypes%22%3Anull%2C%22multiSelection%22%3Afalse%2C%22status%22%3A0%7D&p_p_auth=IRVXzaPX';
+
+const MOCK_DM_ENABLED = true;
+
 const wrapper = fragmentElement;
-
-if (layoutMode === 'edit') {
-	const selectButton = wrapper.querySelector('.btn-secondary');
-
-	if (selectButton) {
-		selectButton.classList.add('disabled');
-	}
-}
 
 const input = document.getElementById(`${fragmentNamespace}-file-upload`);
 const fileName = wrapper.querySelector('.forms-file-upload-file-name');
 const removeButton = wrapper.querySelector("[type='button']");
+const selectButton = wrapper.querySelector('.btn-secondary');
+
+if (layoutMode === 'edit') {
+	if (selectButton) {
+		selectButton.classList.add('disabled');
+	}
+}
 
 function onInputChange() {
 	fileName.innerText = input.files[0].name;
@@ -41,4 +45,22 @@ function onRemoveFile() {
 	removeButton.removeEventListener('click', onRemoveFile);
 }
 
+function onSelectFile(event) {
+	if (MOCK_DM_ENABLED) {
+		event.preventDefault();
+
+		Liferay.Util.openSelectionModal({
+			customSelectEvent: true,
+			multiple: true,
+			onSelect(selectedItem) {
+				console.log(selectedItem);
+			},
+			selectEventName: 'MOCK_SELECT_EVENT_NAME',
+			title: 'Select Document',
+			url: MOCK_URL,
+		});
+	}
+}
+
 input.addEventListener('change', onInputChange);
+selectButton.addEventListener('click', onSelectFile);
