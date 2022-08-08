3ed7b1b38448567396b19513ac7bc18601d63a90
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-08-08 19:21:23 +0200

    LPS-159979 Implement mark and unmark a fragment as highlighted

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdateFragmentsHighlightedConfigurationMVCActionCommand.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdateFragmentsHighlightedConfigurationMVCActionCommand.java
@@ -14,17 +14,63 @@
 
 package com.liferay.layout.content.page.editor.web.internal.portlet.action;
 
+import com.liferay.fragment.constants.FragmentConstants;
+import com.liferay.fragment.contributor.FragmentCollectionContributorTracker;
+import com.liferay.fragment.model.FragmentComposition;
+import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.renderer.FragmentRenderer;
+import com.liferay.fragment.renderer.FragmentRendererTracker;
+import com.liferay.fragment.service.FragmentCompositionService;
+import com.liferay.fragment.service.FragmentEntryLocalService;
 import com.liferay.layout.content.page.editor.constants.ContentPageEditorPortletKeys;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.layout.content.page.editor.web.internal.constants.ContentPageEditorConstants;
+import com.liferay.layout.content.page.editor.web.internal.util.FragmentEntryLinkManager;
+import com.liferay.layout.content.page.editor.web.internal.util.layout.structure.LayoutStructureUtil;
+import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
+import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalService;
+import com.liferay.layout.util.structure.DropZoneLayoutStructureItem;
+import com.liferay.layout.util.structure.LayoutStructure;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.PortletConstants;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.portlet.PortletIdCodec;
+import com.liferay.portal.kernel.portlet.PortletPreferencesFactory;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.segments.constants.SegmentsExperienceConstants;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.portlet.PortletPreferences;
+import javax.portlet.ValidatorException;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Lourdes Fernández Besada
@@ -47,8 +93,301 @@ public class UpdateFragmentsHighlightedConfigurationMVCActionCommand
 
 		JSONPortletResponseUtil.writeJSON(
 			actionRequest, actionResponse,
-			JSONUtil.put(
-				"highlightedFragments", JSONFactoryUtil.createJSONArray()));
+			_updateFragmentsHighlightedConfiguration(actionRequest));
 	}
+
+	private JSONArray _getHighlightedFragmentsJSONArray(
+		Set<String> highlightedFragmentEntryKeys,
+		HttpServletRequest httpServletRequest, ThemeDisplay themeDisplay) {
+
+		DropZoneLayoutStructureItem masterDropZoneLayoutStructureItem =
+			_getMasterDropZoneLayoutStructureItem(themeDisplay);
+
+		Map<String, JSONObject> map = new TreeMap<>();
+
+		for (String key : highlightedFragmentEntryKeys) {
+			int pos = key.indexOf(StringPool.POUND);
+
+			long groupId = 0;
+
+			if (pos > 0) {
+				String groupKey = GetterUtil.getString(key.substring(pos + 1));
+
+				Group group = _groupLocalService.fetchGroup(
+					themeDisplay.getCompanyId(), groupKey);
+
+				if (group != null) {
+					groupId = group.getGroupId();
+				}
+
+				key = key.substring(0, pos);
+			}
+
+			if (!_isAllowedFragmentEntryKey(
+					key, masterDropZoneLayoutStructureItem)) {
+
+				continue;
+			}
+
+			FragmentRenderer fragmentRenderer =
+				_fragmentRendererTracker.getFragmentRenderer(key);
+
+			if (fragmentRenderer != null) {
+				String label = fragmentRenderer.getLabel(
+					themeDisplay.getLocale());
+
+				map.put(
+					label,
+					JSONUtil.put(
+						"fragmentEntryKey", fragmentRenderer.getKey()
+					).put(
+						"highlighted", true
+					).put(
+						"icon", fragmentRenderer.getIcon()
+					).put(
+						"imagePreviewURL",
+						fragmentRenderer.getImagePreviewURL(httpServletRequest)
+					).put(
+						"name", label
+					));
+
+				continue;
+			}
+
+			FragmentEntry fragmentEntry =
+				_fragmentEntryLinkManager.getFragmentEntry(
+					groupId, key, themeDisplay.getLocale());
+
+			if (fragmentEntry != null) {
+				map.put(
+					fragmentEntry.getName(),
+					JSONUtil.put(
+						"fragmentEntryKey", fragmentEntry.getFragmentEntryKey()
+					).put(
+						"groupId", fragmentEntry.getGroupId()
+					).put(
+						"icon", fragmentEntry.getIcon()
+					).put(
+						"imagePreviewURL",
+						fragmentEntry.getImagePreviewURL(themeDisplay)
+					).put(
+						"name", fragmentEntry.getName()
+					).put(
+						"type",
+						FragmentConstants.getTypeLabel(fragmentEntry.getType())
+					));
+
+				continue;
+			}
+
+			FragmentComposition fragmentComposition =
+				_fragmentCollectionContributorTracker.getFragmentComposition(
+					key);
+
+			if (fragmentComposition == null) {
+				fragmentComposition =
+					_fragmentCompositionService.fetchFragmentComposition(
+						groupId, key);
+			}
+
+			if (fragmentComposition == null) {
+				continue;
+			}
+
+			map.put(
+				fragmentEntry.getName(),
+				JSONUtil.put(
+					"fragmentEntryKey",
+					fragmentComposition.getFragmentCompositionKey()
+				).put(
+					"groupId", fragmentComposition.getGroupId()
+				).put(
+					"icon", fragmentComposition.getIcon()
+				).put(
+					"imagePreviewURL",
+					fragmentComposition.getImagePreviewURL(themeDisplay)
+				).put(
+					"name", fragmentComposition.getName()
+				).put(
+					"type", ContentPageEditorConstants.TYPE_COMPOSITION
+				));
+		}
+
+		List<JSONObject> sortedHighlightedFragments = new ArrayList<>(
+			map.values());
+
+		return JSONUtil.putAll(
+			sortedHighlightedFragments.toArray(new JSONObject[0]));
+	}
+
+	private DropZoneLayoutStructureItem _getMasterDropZoneLayoutStructureItem(
+		ThemeDisplay themeDisplay) {
+
+		Layout layout = themeDisplay.getLayout();
+
+		if (layout.getMasterLayoutPlid() <= 0) {
+			return null;
+		}
+
+		LayoutPageTemplateEntry masterLayoutPageTemplateEntry =
+			_layoutPageTemplateEntryLocalService.
+				fetchLayoutPageTemplateEntryByPlid(
+					layout.getMasterLayoutPlid());
+
+		if (masterLayoutPageTemplateEntry == null) {
+			return null;
+		}
+
+		try {
+			LayoutStructure masterLayoutStructure =
+				LayoutStructureUtil.getLayoutStructure(
+					masterLayoutPageTemplateEntry.getGroupId(),
+					masterLayoutPageTemplateEntry.getPlid(),
+					SegmentsExperienceConstants.KEY_DEFAULT);
+
+			return (DropZoneLayoutStructureItem)
+				masterLayoutStructure.getDropZoneLayoutStructureItem();
+		}
+		catch (Exception exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug("Unable to get master layout structure", exception);
+			}
+		}
+
+		return null;
+	}
+
+	private boolean _isAllowedFragmentEntryKey(
+		String fragmentEntryKey,
+		DropZoneLayoutStructureItem masterDropZoneLayoutStructureItem) {
+
+		if (masterDropZoneLayoutStructureItem == null) {
+			return true;
+		}
+
+		List<String> fragmentEntryKeys =
+			masterDropZoneLayoutStructureItem.getFragmentEntryKeys();
+
+		if (masterDropZoneLayoutStructureItem.isAllowNewFragmentEntries()) {
+			if (ListUtil.isEmpty(fragmentEntryKeys) ||
+				!fragmentEntryKeys.contains(fragmentEntryKey)) {
+
+				return true;
+			}
+
+			return false;
+		}
+
+		if (ListUtil.isNotEmpty(fragmentEntryKeys) &&
+			fragmentEntryKeys.contains(fragmentEntryKey)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	private JSONObject _updateFragmentsHighlightedConfiguration(
+			ActionRequest actionRequest)
+		throws Exception {
+
+		String fragmentEntryKey = ParamUtil.getString(
+			actionRequest, "fragmentEntryKey");
+
+		HttpServletRequest httpServletRequest = _portal.getHttpServletRequest(
+			actionRequest);
+
+		if (Validator.isNull(fragmentEntryKey)) {
+			hideDefaultSuccessMessage(actionRequest);
+
+			return JSONUtil.put(
+				"error",
+				_language.get(
+					httpServletRequest, "an-unexpected-error-occurred"));
+		}
+
+		boolean highlighted = ParamUtil.getBoolean(
+			actionRequest, "highlighted");
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		PortletPreferences portletPreferences =
+			_portletPreferencesFactory.getPortletSetup(
+				httpServletRequest,
+				PortletIdCodec.encode(
+					ContentPageEditorPortletKeys.CONTENT_PAGE_EDITOR_PORTLET,
+					themeDisplay.getUserId()),
+				PortletConstants.DEFAULT_PREFERENCES);
+
+		Set<String> highlightedFragmentEntryKeys = SetUtil.fromArray(
+			portletPreferences.getValues(
+				"highlightedFragmentEntryKeys", new String[0]));
+
+		if (highlighted) {
+			highlightedFragmentEntryKeys.add(fragmentEntryKey);
+		}
+		else {
+			highlightedFragmentEntryKeys.remove(fragmentEntryKey);
+		}
+
+		portletPreferences.setValues(
+			"highlightedFragmentEntryKeys",
+			highlightedFragmentEntryKeys.toArray(new String[0]));
+
+		try {
+			portletPreferences.store();
+		}
+		catch (ValidatorException validatorException) {
+			hideDefaultSuccessMessage(actionRequest);
+
+			return JSONUtil.put(
+				"error",
+				_language.get(
+					httpServletRequest, "an-unexpected-error-occurred"));
+		}
+
+		return JSONUtil.put(
+			"highlightedFragments",
+			_getHighlightedFragmentsJSONArray(
+				highlightedFragmentEntryKeys, httpServletRequest,
+				themeDisplay));
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		UpdateFragmentsHighlightedConfigurationMVCActionCommand.class);
+
+	@Reference
+	private FragmentCollectionContributorTracker
+		_fragmentCollectionContributorTracker;
+
+	@Reference
+	private FragmentCompositionService _fragmentCompositionService;
+
+	@Reference
+	private FragmentEntryLinkManager _fragmentEntryLinkManager;
+
+	@Reference
+	private FragmentEntryLocalService _fragmentEntryLocalService;
+
+	@Reference
+	private FragmentRendererTracker _fragmentRendererTracker;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private LayoutPageTemplateEntryLocalService
+		_layoutPageTemplateEntryLocalService;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private PortletPreferencesFactory _portletPreferencesFactory;
 
 }
