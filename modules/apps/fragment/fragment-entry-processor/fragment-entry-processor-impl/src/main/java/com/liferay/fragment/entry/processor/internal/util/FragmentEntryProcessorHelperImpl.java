f911e78ba25eade60fb8ee90d0c387e81256f40c
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2022-10-26 17:16:49 +0200

    LPS-165478 SF sort

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-impl/src/main/java/com/liferay/fragment/entry/processor/internal/util/FragmentEntryProcessorHelperImpl.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-impl/src/main/java/com/liferay/fragment/entry/processor/internal/util/FragmentEntryProcessorHelperImpl.java
@@ -79,8 +79,6 @@ public class FragmentEntryProcessorHelperImpl
 			return null;
 		}
 
-		String formattedFieldValue;
-
 		if (fieldValue instanceof Collection) {
 			Collection<Object> collection = (Collection<Object>)fieldValue;
 
@@ -99,10 +97,16 @@ public class FragmentEntryProcessorHelperImpl
 			InfoCollectionTextFormatter<Object> infoCollectionTextFormatter =
 				_getInfoCollectionTextFormatter(itemClassName);
 
-			formattedFieldValue = infoCollectionTextFormatter.format(
-				collection, locale);
+			return infoCollectionTextFormatter.format(collection, locale);
 		}
-		else if (fieldValue instanceof String) {
+
+		if (fieldValue instanceof Labeled) {
+			Labeled labeledFieldValue = (Labeled)fieldValue;
+
+			return labeledFieldValue.getLabel(locale);
+		}
+
+		if (fieldValue instanceof String) {
 			InfoField infoField = infoFieldValue.getInfoField();
 
 			InfoFieldType infoFieldType = infoField.getInfoFieldType();
@@ -129,52 +133,37 @@ public class FragmentEntryProcessorHelperImpl
 					optionsOptional.orElseGet(null);
 
 				if (optionValuesJSONArray == null) {
-					formattedFieldValue = _getOptionLabel(
-						locale, options, optionValues);
+					return _getOptionLabel(locale, options, optionValues);
 				}
-				else {
-					String[] optionLabels =
-						new String[optionValuesJSONArray.length()];
 
-					for (int i = 0; i < optionValuesJSONArray.length(); i++) {
-						optionLabels[i] = _getOptionLabel(
-							locale, options,
-							optionValuesJSONArray.getString(i));
-					}
+				String[] optionLabels =
+					new String[optionValuesJSONArray.length()];
 
-					formattedFieldValue = StringUtil.merge(
-						optionLabels, StringPool.COMMA);
+				for (int i = 0; i < optionValuesJSONArray.length(); i++) {
+					optionLabels[i] = _getOptionLabel(
+						locale, options, optionValuesJSONArray.getString(i));
 				}
-			}
-			else {
-				formattedFieldValue = (String)fieldValue;
-			}
-		}
-		else if (fieldValue instanceof Labeled) {
-			Labeled labeledFieldValue = (Labeled)fieldValue;
 
-			formattedFieldValue = labeledFieldValue.getLabel(locale);
-		}
-		else {
-			Class<?> fieldValueClass = fieldValue.getClass();
-
-			String itemClassName = fieldValueClass.getName();
-
-			InfoTextFormatter<Object> infoTextFormatter =
-				(InfoTextFormatter<Object>)
-					_infoItemServiceTracker.getFirstInfoItemService(
-						InfoTextFormatter.class, itemClassName);
-
-			if (infoTextFormatter == null) {
-				formattedFieldValue = fieldValue.toString();
+				return StringUtil.merge(optionLabels, StringPool.COMMA);
 			}
-			else {
-				formattedFieldValue = infoTextFormatter.format(
-					fieldValue, locale);
-			}
+
+			return (String)fieldValue;
 		}
 
-		return formattedFieldValue;
+		Class<?> fieldValueClass = fieldValue.getClass();
+
+		String itemClassName = fieldValueClass.getName();
+
+		InfoTextFormatter<Object> infoTextFormatter =
+			(InfoTextFormatter<Object>)
+				_infoItemServiceTracker.getFirstInfoItemService(
+					InfoTextFormatter.class, itemClassName);
+
+		if (infoTextFormatter == null) {
+			return fieldValue.toString();
+		}
+
+		return infoTextFormatter.format(fieldValue, locale);
 	}
 
 	@Override
