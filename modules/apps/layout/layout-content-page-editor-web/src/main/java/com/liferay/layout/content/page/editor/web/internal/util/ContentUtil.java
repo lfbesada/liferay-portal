4c6f710d7ccb9d5587b57e1ecf096812e8028878
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-02-01 12:12:35 +0100

    LPS-177533 SF, add to the list only the itemId, we have the layoutStructure to easily get the LayoutStructureItem, and in this way it will consume less memory

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/util/ContentUtil.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/util/ContentUtil.java
@@ -77,7 +77,6 @@ import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.JavaConstants;
-import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -159,11 +158,11 @@ public class ContentUtil {
 			LayoutStructureUtil.getLayoutStructure(
 				themeDisplay.getScopeGroupId(), plid, segmentsExperienceId);
 
-		List<LayoutStructureItem> restrictedLayoutStructureItems =
-			_getRestrictedLayoutStructureItems(layoutStructure, themeDisplay);
+		List<String> restrictedItemIds = _getRestrictedItemIds(
+			layoutStructure, themeDisplay);
 
 		List<String> hiddenItemIds = _getHiddenItemIds(
-			layoutStructure, restrictedLayoutStructureItems);
+			layoutStructure, restrictedItemIds);
 
 		return JSONUtil.concat(
 			_getLayoutClassedModelPageContentsJSONArray(
@@ -171,10 +170,7 @@ public class ContentUtil {
 				segmentsExperienceId),
 			AssetListEntryUsagesUtil.getPageContentsJSONArray(
 				hiddenItemIds, httpServletRequest, httpServletResponse,
-				layoutStructure, plid,
-				ListUtil.toList(
-					restrictedLayoutStructureItems,
-					layoutStructureItem -> layoutStructureItem.getItemId())));
+				layoutStructure, plid, restrictedItemIds));
 	}
 
 	@Reference(unbind = "-")
@@ -339,10 +335,16 @@ public class ContentUtil {
 	}
 
 	private static List<String> _getChildrenItemIds(
-		LayoutStructure layoutStructure,
-		LayoutStructureItem layoutStructureItem) {
+		String itemId, LayoutStructure layoutStructure) {
 
 		List<String> childrenItemIds = new ArrayList<>();
+
+		LayoutStructureItem layoutStructureItem =
+			layoutStructure.getLayoutStructureItem(itemId);
+
+		if (layoutStructureItem == null) {
+			return childrenItemIds;
+		}
 
 		for (String childItemId : layoutStructureItem.getChildrenItemIds()) {
 			childrenItemIds.add(childItemId);
@@ -351,7 +353,8 @@ public class ContentUtil {
 				layoutStructure.getLayoutStructureItem(childItemId);
 
 			childrenItemIds.addAll(
-				_getChildrenItemIds(layoutStructure, childLayoutStructureItem));
+				_getChildrenItemIds(
+					childLayoutStructureItem.getItemId(), layoutStructure));
 		}
 
 		return childrenItemIds;
@@ -497,17 +500,13 @@ public class ContentUtil {
 	}
 
 	private static List<String> _getHiddenItemIds(
-		LayoutStructure layoutStructure,
-		List<LayoutStructureItem> restrictedLayoutStructureItems) {
+		LayoutStructure layoutStructure, List<String> restrictedItemIds) {
 
 		List<String> hiddenItemIds = new ArrayList<>();
 
-		for (LayoutStructureItem restrictedLayoutStructureItem :
-				restrictedLayoutStructureItems) {
-
+		for (String restrictedItemId : restrictedItemIds) {
 			hiddenItemIds.addAll(
-				_getChildrenItemIds(
-					layoutStructure, restrictedLayoutStructureItem));
+				_getChildrenItemIds(restrictedItemId, layoutStructure));
 		}
 
 		return hiddenItemIds;
@@ -855,14 +854,13 @@ public class ContentUtil {
 		return _portletClassNameId;
 	}
 
-	private static List<LayoutStructureItem> _getRestrictedLayoutStructureItems(
+	private static List<String> _getRestrictedItemIds(
 		LayoutStructure layoutStructure, ThemeDisplay themeDisplay) {
 
-		List<LayoutStructureItem> restrictedLayoutStructureItems =
-			new ArrayList<>();
+		List<String> restrictedItemIds = new ArrayList<>();
 
 		if (!FeatureFlagManagerUtil.isEnabled("LPS-169923")) {
-			return restrictedLayoutStructureItems;
+			return restrictedItemIds;
 		}
 
 		for (FormStyledLayoutStructureItem formStyledLayoutStructureItem :
@@ -891,7 +889,7 @@ public class ContentUtil {
 				continue;
 			}
 
-			restrictedLayoutStructureItems.add(formStyledLayoutStructureItem);
+			restrictedItemIds.add(formStyledLayoutStructureItem.getItemId());
 		}
 
 		for (CollectionStyledLayoutStructureItem
@@ -946,11 +944,11 @@ public class ContentUtil {
 				continue;
 			}
 
-			restrictedLayoutStructureItems.add(
-				collectionStyledLayoutStructureItem);
+			restrictedItemIds.add(
+				collectionStyledLayoutStructureItem.getItemId());
 		}
 
-		return restrictedLayoutStructureItems;
+		return restrictedItemIds;
 	}
 
 	private static List<String> _getRestrictedPortletIds(
