5c389dbd042ea632c355c786a2e988c2bdaecd05
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-02-13 14:22:51 +0100

    LPS-175543 Adapt AssetSearcher usages

--- modules/apps/asset/asset-service/src/main/java/com/liferay/asset/internal/info/collection/provider/AssetEntriesWithSameAssetCategoryRelatedInfoItemCollectionProvider.java
+++ modules/apps/asset/asset-service/src/main/java/com/liferay/asset/internal/info/collection/provider/AssetEntriesWithSameAssetCategoryRelatedInfoItemCollectionProvider.java
@@ -18,6 +18,7 @@ import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
 import com.liferay.asset.kernel.model.AssetCategory;
 import com.liferay.asset.kernel.model.AssetEntry;
 import com.liferay.asset.kernel.model.AssetRendererFactory;
+import com.liferay.asset.kernel.search.AssetSearcherFactory;
 import com.liferay.asset.kernel.service.AssetCategoryLocalService;
 import com.liferay.asset.kernel.service.persistence.AssetEntryQuery;
 import com.liferay.asset.util.AssetHelper;
@@ -49,6 +50,7 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactoryUtil;
 import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.search.BaseSearcher;
 import com.liferay.portal.kernel.search.BooleanClause;
 import com.liferay.portal.kernel.search.BooleanClauseFactoryUtil;
 import com.liferay.portal.kernel.search.BooleanClauseOccur;
@@ -77,7 +79,6 @@ import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.Tuple;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
-import com.liferay.portlet.asset.util.AssetSearcher;
 
 import java.io.Serializable;
 
@@ -505,10 +506,8 @@ public class AssetEntriesWithSameAssetCategoryRelatedInfoItemCollectionProvider
 		assetEntryQuery.setAllCategoryIds(allCategoryIds);
 		assetEntryQuery.setAnyCategoryIds(anyCategoryIds);
 
-		AssetSearcher assetSearcher =
-			(AssetSearcher)AssetSearcher.getInstance();
-
-		assetSearcher.setAssetEntryQuery(assetEntryQuery);
+		BaseSearcher assetSearcher = _assetSearcherFactory.createAssetSearcher(
+			assetEntryQuery);
 
 		BooleanQuery booleanQuery = assetSearcher.getFullQuery(searchContext);
 
@@ -669,6 +668,9 @@ public class AssetEntriesWithSameAssetCategoryRelatedInfoItemCollectionProvider
 
 	@Reference
 	private AssetHelper _assetHelper;
+
+	@Reference
+	private AssetSearcherFactory _assetSearcherFactory;
 
 	@Reference
 	private ItemSelector _itemSelector;
