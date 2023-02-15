10beebbf012166696756c21909848cb3ae8d6a92
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-02-15 09:31:11 +0100

    LPS-149968 Update JournalArticleIndexer references

--- modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/indexer/clauses/test/IndexerClausesComplexQueryPartTest.java
+++ modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/indexer/clauses/test/IndexerClausesComplexQueryPartTest.java
@@ -364,7 +364,9 @@ public class IndexerClausesComplexQueryPartTest {
 	@Inject
 	protected BlogsEntryLocalService blogsEntryLocalService;
 
-	@Inject(filter = "component.name=*.JournalArticleIndexer")
+	@Inject(
+		filter = "indexer.class.name=com.liferay.journal.model.JournalArticle"
+	)
 	protected Indexer<JournalArticle> journalArticleIndexer;
 
 	@Inject
