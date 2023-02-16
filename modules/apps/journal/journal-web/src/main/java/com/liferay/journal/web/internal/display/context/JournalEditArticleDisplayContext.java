a374595d66e856413ea2151034be68f66c35c86e
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-02-16 17:38:52 +0100

    LPS-175555 Displays a duplicated URL warning message when rendering the article edition view if applied. Logic copied from UpdateArticleMVCActionCommand, only changing the language keys.

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalEditArticleDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalEditArticleDisplayContext.java
@@ -29,6 +29,7 @@ import com.liferay.dynamic.data.mapping.service.DDMTemplateLocalServiceUtil;
 import com.liferay.dynamic.data.mapping.service.DDMTemplateServiceUtil;
 import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
 import com.liferay.dynamic.data.mapping.util.DDMFormValuesToMapConverter;
+import com.liferay.friendly.url.service.FriendlyURLEntryLocalServiceUtil;
 import com.liferay.item.selector.ItemSelector;
 import com.liferay.item.selector.ItemSelectorCriterion;
 import com.liferay.item.selector.criteria.URLItemSelectorReturnType;
@@ -670,6 +671,102 @@ public class JournalEditArticleDisplayContext {
 		return sb.toString();
 	}
 
+	public String getFriendlyURLDuplicatedWarningMessage()
+		throws PortalException {
+
+		if (_friendlyURLDuplicatedWarningMessage != null) {
+			return _friendlyURLDuplicatedWarningMessage;
+		}
+
+		if (_article == null) {
+			_friendlyURLDuplicatedWarningMessage = StringPool.BLANK;
+
+			return _friendlyURLDuplicatedWarningMessage;
+		}
+
+		List<Locale> friendlyURLDuplicatedLocales = new ArrayList<>();
+		Map<String, List<Long>> friendlyURLGroupIdsMap = new HashMap<>();
+
+		Map<Locale, String> friendlyURLMap = _article.getFriendlyURLMap();
+
+		for (Map.Entry<Locale, String> entry : friendlyURLMap.entrySet()) {
+			List<Long> groupIds = friendlyURLGroupIdsMap.computeIfAbsent(
+				entry.getValue(),
+				key -> FriendlyURLEntryLocalServiceUtil.getGroupIdsByUrlTitle(
+					PortalUtil.getClassNameId(JournalArticle.class),
+					_themeDisplay.getCompanyId(), key));
+
+			if (!groupIds.isEmpty() &&
+				((groupIds.size() > 1) ||
+				 !Objects.equals(
+					 groupIds.get(0), _themeDisplay.getScopeGroupId()))) {
+
+				friendlyURLDuplicatedLocales.add(entry.getKey());
+			}
+		}
+
+		if (friendlyURLDuplicatedLocales.isEmpty()) {
+			_friendlyURLDuplicatedWarningMessage = StringPool.BLANK;
+
+			return _friendlyURLDuplicatedWarningMessage;
+		}
+
+		String friendlyURLDuplicatedWarningMessage;
+
+		if (friendlyURLDuplicatedLocales.size() > 3) {
+			friendlyURLDuplicatedWarningMessage = LanguageUtil.format(
+				_themeDisplay.getLocale(),
+				"the-url-used-in-x-and-x-more-translations-already-exists-in-" +
+					"other-sites-or-asset-libraries",
+				new String[] {
+					_getLocaleDisplayNames(
+						_themeDisplay.getLocale(),
+						friendlyURLDuplicatedLocales.get(0),
+						friendlyURLDuplicatedLocales.get(1),
+						friendlyURLDuplicatedLocales.get(2)),
+					String.valueOf(friendlyURLDuplicatedLocales.size() - 3)
+				},
+				false);
+		}
+		else if (friendlyURLDuplicatedLocales.size() > 1) {
+			int lastElementIndex = friendlyURLDuplicatedLocales.size() - 1;
+
+			List<Locale> locales = ListUtil.subList(
+				friendlyURLDuplicatedLocales, 0, lastElementIndex);
+
+			friendlyURLDuplicatedWarningMessage = LanguageUtil.format(
+				_themeDisplay.getLocale(),
+				"the-url-used-in-x-and-x-already-exists-in-other-sites-or-" +
+					"asset-libraries",
+				new String[] {
+					_getLocaleDisplayNames(
+						_themeDisplay.getLocale(),
+						locales.toArray(new Locale[0])),
+					_getLocaleDisplayNames(
+						_themeDisplay.getLocale(),
+						friendlyURLDuplicatedLocales.get(lastElementIndex))
+				},
+				false);
+		}
+		else {
+			friendlyURLDuplicatedWarningMessage = LanguageUtil.format(
+				_themeDisplay.getLocale(),
+				"the-url-used-in-x-already-exists-in-other-sites-or-asset-" +
+					"libraries",
+				new String[] {
+					_getLocaleDisplayNames(
+						_themeDisplay.getLocale(),
+						friendlyURLDuplicatedLocales.get(0))
+				},
+				false);
+		}
+
+		_friendlyURLDuplicatedWarningMessage =
+			friendlyURLDuplicatedWarningMessage;
+
+		return _friendlyURLDuplicatedWarningMessage;
+	}
+
 	public long getGroupId() {
 		if (_groupId != null) {
 			return _groupId;
@@ -1150,6 +1247,17 @@ public class JournalEditArticleDisplayContext {
 			_article, _httpServletRequest, "layoutUuid", null);
 	}
 
+	private String _getLocaleDisplayNames(Locale locale, Locale... locales) {
+		List<String> displayLocaleNames = new ArrayList<>();
+
+		for (Locale currentLocale : locales) {
+			displayLocaleNames.add(
+				LocaleUtil.getLocaleDisplayName(currentLocale, locale));
+		}
+
+		return StringUtil.merge(displayLocaleNames, StringPool.COMMA_AND_SPACE);
+	}
+
 	private String _getSelectAssetDisplayPageURL(
 		String selectAssetDisplayPageEventName, boolean showPortletLayouts) {
 
@@ -1378,6 +1486,7 @@ public class JournalEditArticleDisplayContext {
 		_ffJournalAutoSaveDraftConfiguration;
 	private Long _folderId;
 	private String _folderName;
+	private String _friendlyURLDuplicatedWarningMessage;
 	private Long _groupId;
 	private final HttpServletRequest _httpServletRequest;
 	private Long _inheritedWorkflowDDMStructuresFolderId;
