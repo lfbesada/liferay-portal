1d869fb426854b072fc8dffcb2ed4f4dbd04231e
Jorge Díaz <jorge.diaz@liferay.com>
2023-02-17 14:16:03 +0100

    LPS-176274 Simplify, first call always return null for resourcePrimKey

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/search/JournalArticleDDMStructureIndexer.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/search/JournalArticleDDMStructureIndexer.java
@@ -130,14 +130,8 @@ public class JournalArticleDDMStructureIndexer implements DDMStructureIndexer {
 			actionableDynamicQuery.setPerformActionMethod(
 				(JournalArticleResource journalArticleResource) -> {
 					JournalArticle journalArticle =
-						_journalArticleLocalService.fetchJournalArticle(
+						_journalArticleLocalService.fetchLatestArticle(
 							journalArticleResource.getResourcePrimKey());
-
-					if (journalArticle == null) {
-						journalArticle =
-							_journalArticleLocalService.fetchLatestArticle(
-								journalArticleResource.getResourcePrimKey());
-					}
 
 					try {
 						indexer.reindex(journalArticle);
