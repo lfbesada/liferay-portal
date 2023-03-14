3c6098a8a8e09736030bbf46c5617540d84b79d6
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-03-14 12:19:01 +0100

    LPS-178316 Allow using ancestor site's structures

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -1990,7 +1990,8 @@ public class BundleSiteInitializer implements SiteInitializer {
 
 			ddmStructureLocalService.getStructure(
 				serviceContext.getScopeGroupId(),
-				_portal.getClassNameId(JournalArticle.class), ddmStructureKey);
+				_portal.getClassNameId(JournalArticle.class), ddmStructureKey,
+				true);
 
 			String ddmTemplateKey = jsonObject.getString("ddmTemplateKey");
 
