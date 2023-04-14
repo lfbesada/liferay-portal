4cd70275efe0ed868e8ef0f5fa1d36305aa0f4c8
mikellorza <mikel.lorza@liferay.com>
2023-04-14 13:20:30 +0200

    LPS-183358 Add FF LPS-176136

--- modules/apps/site/site-api/src/main/java/com/liferay/site/manager/MenuAccessManager.java
+++ modules/apps/site/site-api/src/main/java/com/liferay/site/manager/MenuAccessManager.java
@@ -14,6 +14,7 @@
 
 package com.liferay.site.manager;
 
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Layout;
@@ -37,6 +38,10 @@ public class MenuAccessManager {
 
 	public static boolean isShowControlMenu(
 		HttpServletRequest httpServletRequest) {
+
+		if (!FeatureFlagManagerUtil.isEnabled("LPS-176136")) {
+			return true;
+		}
 
 		ThemeDisplay themeDisplay =
 			(ThemeDisplay)httpServletRequest.getAttribute(
