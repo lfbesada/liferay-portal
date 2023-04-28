dfe2dd76f4f8c140a68178a5c2b1e468c15827d3
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-04-28 15:17:00 +0100

    LPS-183059 Add AI-Creator WebContent group configuration

---
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/configuration/JournalArticleAICreatorGroupConfiguration.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.web.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@ExtendedObjectClassDefinition(
+	category = "ai-creator", scope = ExtendedObjectClassDefinition.Scope.GROUP
+)
+@Meta.OCD(
+	description = "journal-article-ai-creator-group-configuration-description",
+	id = "com.liferay.journal.web.internal.configuration.JournalArticleAICreatorGroupConfiguration",
+	localization = "content/Language",
+	name = "journal-article-ai-creator-group-configuration-name"
+)
+public interface JournalArticleAICreatorGroupConfiguration {
+
+	@Meta.AD(
+		deflt = "true", name = "enable-openai-to-create-content-in-your-sites",
+		required = false
+	)
+	public boolean enableOpenAIToCreateContentInYourSites();
+
+	@Meta.AD(deflt = "", name = "api-key", required = false)
+	public String apiKey();
+
+}
