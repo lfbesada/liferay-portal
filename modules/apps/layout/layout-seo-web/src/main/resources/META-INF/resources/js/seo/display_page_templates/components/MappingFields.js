f4ea27fae7e15723b631fb5daa42245a5e22ff34
Sandro Chinea <sandrochinea@gmail.com>
2023-05-30 09:52:01 +0100

    LPS-184932 Change to array is it will support more than one type

--- modules/apps/layout/layout-seo-web/src/main/resources/META-INF/resources/js/seo/display_page_templates/components/MappingFields.js
+++ modules/apps/layout/layout-seo-web/src/main/resources/META-INF/resources/js/seo/display_page_templates/components/MappingFields.js
@@ -20,22 +20,22 @@ import MappingInput from './MappingInput';
 import MappingSelector from './MappingSelector';
 
 function MappingFields({fields, inputs, selectedSource}) {
-	return inputs.map((props) => {
-		const filteredFields = fields.filter(
-			({type}) => type === props.fieldType
+	return inputs.map(({fieldTypes = [], name, ...props}) => {
+		const filteredFields = fields.filter(({type}) =>
+			fieldTypes.includes(type)
 		);
 
-		return props.fieldType === FIELD_TYPES.TEXT ? (
+		return fieldTypes.includes(FIELD_TYPES.TEXT) ? (
 			<MappingInput
 				fields={filteredFields}
-				key={props.name}
+				key={name}
 				selectedSource={selectedSource}
 				{...props}
 			/>
 		) : (
 			<MappingSelector
 				fields={filteredFields}
-				key={props.name}
+				key={name}
 				selectedSource={selectedSource}
 				{...props}
 			/>
@@ -52,6 +52,7 @@ MappingFields.propTypes = {
 	).isRequired,
 	inputs: PropTypes.arrayOf(
 		PropTypes.shape({
+			fieldTypes: PropTypes.array,
 			label: PropTypes.string,
 			name: PropTypes.string,
 			selectedFieldKey: PropTypes.string,
