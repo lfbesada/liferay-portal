87654d86b560024536536199c5bc5f1bc6f803a8
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-06-08 09:44:15 +0200

    LPS-186596 Adds to the editor context a boolean to determine if there set an API Key to be used for the site or not

--- modules/apps/ai-creator/ai-creator-openai-web/src/main/java/com/liferay/ai/creator/openai/web/internal/editor/configuration/AICreatorOpenAIEditorConfigContributor.java
+++ modules/apps/ai-creator/ai-creator-openai-web/src/main/java/com/liferay/ai/creator/openai/web/internal/editor/configuration/AICreatorOpenAIEditorConfigContributor.java
@@ -28,6 +28,7 @@ import com.liferay.portal.kernel.module.configuration.ConfigurationException;
 import com.liferay.portal.kernel.portlet.LiferayWindowState;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Map;
 
@@ -91,6 +92,27 @@ public class AICreatorOpenAIEditorConfigContributor
 				portletURL.setParameter("mvcPath", "/view.jsp");
 
 				return portletURL.toString();
+			}
+		).put(
+			"isAICreatorOpenAIAPIKey",
+			() -> {
+				try {
+					if (Validator.isNotNull(
+							_aiCreatorOpenAIConfigurationManager.
+								getAICreatorOpenAIGroupAPIKey(
+									themeDisplay.getCompanyId(),
+									themeDisplay.getScopeGroupId()))) {
+
+						return true;
+					}
+				}
+				catch (ConfigurationException configurationException) {
+					if (_log.isDebugEnabled()) {
+						_log.debug(configurationException);
+					}
+				}
+
+				return false;
 			}
 		);
 	}
