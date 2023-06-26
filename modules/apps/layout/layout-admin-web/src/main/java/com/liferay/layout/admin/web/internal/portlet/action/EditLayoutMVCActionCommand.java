fd6b2dc2c64baf407082aa0a29299f4c8aa0de40
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-06-26 07:41:56 +0200

    LPS-188303 Add the friendly URL warning message if applies when we are adding or updating a page

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/EditLayoutMVCActionCommand.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/EditLayoutMVCActionCommand.java
@@ -21,6 +21,7 @@ import com.liferay.client.extension.model.ClientExtensionEntryRel;
 import com.liferay.client.extension.service.ClientExtensionEntryRelLocalService;
 import com.liferay.document.library.kernel.service.DLAppLocalService;
 import com.liferay.layout.admin.constants.LayoutAdminPortletKeys;
+import com.liferay.layout.set.prototype.helper.LayoutSetPrototypeHelper;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.events.EventsProcessorUtil;
 import com.liferay.portal.kernel.exception.ModelListenerException;
@@ -289,6 +290,9 @@ public class EditLayoutMVCActionCommand extends BaseMVCActionCommand {
 			MultiSessionMessages.add(
 				actionRequest, portletResource + "layoutUpdated", layout);
 
+			ActionUtil.addFriendlyURLWarningSessionMessages(
+				layout, actionRequest, _layoutSetPrototypeHelper);
+
 			actionRequest.setAttribute(WebKeys.REDIRECT, redirect);
 		}
 		catch (ModelListenerException modelListenerException) {
@@ -424,6 +428,9 @@ public class EditLayoutMVCActionCommand extends BaseMVCActionCommand {
 
 	@Reference
 	private LayoutService _layoutService;
+
+	@Reference
+	private LayoutSetPrototypeHelper _layoutSetPrototypeHelper;
 
 	@Reference
 	private Localization _localization;
