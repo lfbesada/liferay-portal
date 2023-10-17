81ba3682d56f16b6effd96c4aadf8bb0866c1c05
Sandro Chinea <sandrochinea@gmail.com>
2023-10-17 11:33:08 +0100

    LPS-197957 Add thunks to add, update and delete a rule

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/services/RulesService.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/services/RulesService.ts
@@ -3,15 +3,111 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {Action} from '../../plugins/page_rules/components/Action';
+import {Condition} from '../../plugins/page_rules/components/Condition';
+import {LayoutData} from '../../types/layout_data/LayoutData';
 import {config} from '../config/index';
+import draftServiceFetch from './draftServiceFetch';
 import serviceFetch from './serviceFetch';
 
-export default {
-
-	/**
-	 * Get users
-	 */
-	getUsers(): Promise<Array<{screenName: string; userId: string}>> {
-		return serviceFetch(config.getUsersURL, {});
-	},
+/**
+ * Add a rule
+ */
+type AddRuleProps = {
+	actions: Action[];
+	conditions: Condition[];
+	name: string;
+	onNetworkStatus: () => {};
+	segmentsExperienceId: string;
 };
+
+function addRule({
+	actions,
+	conditions,
+	name,
+	onNetworkStatus,
+	segmentsExperienceId,
+}: AddRuleProps): Promise<Array<{layoutData: LayoutData}>> {
+	return draftServiceFetch(
+		config.addRuleURL,
+		{
+			body: {
+				actions: JSON.stringify(actions),
+				conditions: JSON.stringify(conditions),
+				name,
+				segmentsExperienceId,
+			},
+		},
+		onNetworkStatus
+	);
+}
+
+/**
+ * Delete a rule
+ */
+type DeleteRuleProps = {
+	onNetworkStatus: () => {};
+	ruleId: string;
+	segmentsExperienceId: string;
+};
+
+function deleteRule({
+	onNetworkStatus,
+	ruleId,
+	segmentsExperienceId,
+}: DeleteRuleProps): Promise<Array<{layoutData: LayoutData}>> {
+	return draftServiceFetch(
+		config.deleteRuleURL,
+		{
+			body: {
+				ruleId,
+				segmentsExperienceId,
+			},
+		},
+		onNetworkStatus
+	);
+}
+
+/**
+ * Get users
+ */
+function getUsers(): Promise<Array<{screenName: string; userId: string}>> {
+	return serviceFetch(config.getUsersURL, {});
+}
+
+/**
+ * Update a rule with new name, actions and conditions
+ */
+type UpdateRuleProps = {
+	actions: Action[];
+	conditions: Condition[];
+	name: string;
+	onNetworkStatus: () => {};
+	ruleId: string;
+	segmentsExperienceId: string;
+};
+
+function updateRule({
+	actions,
+	conditions,
+	name,
+	onNetworkStatus,
+	ruleId,
+	segmentsExperienceId,
+}: UpdateRuleProps): Promise<Array<{layoutData: LayoutData}>> {
+	return draftServiceFetch(
+		config.updateRuleURL,
+		{
+			body: {
+				actions: JSON.stringify(actions),
+				conditions: JSON.stringify(conditions),
+				name,
+				ruleId,
+				segmentsExperienceId,
+			},
+		},
+		onNetworkStatus
+	);
+}
+
+export default {addRule, deleteRule, getUsers, updateRule};
