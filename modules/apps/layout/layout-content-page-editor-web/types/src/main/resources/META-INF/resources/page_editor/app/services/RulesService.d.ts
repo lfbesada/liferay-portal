c710ea76fc0a41efeb7ce7588dd825da01e3efa8
Victor Galan <victor.galan@liferay.com>
2023-10-23 11:32:04 +0200

    LPS-197957 Types

--- modules/apps/layout/layout-content-page-editor-web/types/src/main/resources/META-INF/resources/page_editor/app/services/RulesService.d.ts
+++ modules/apps/layout/layout-content-page-editor-web/types/src/main/resources/META-INF/resources/page_editor/app/services/RulesService.d.ts
@@ -3,16 +3,87 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-declare const _default: {
+import {Action} from '../../plugins/page_rules/components/Action';
+import {Condition} from '../../plugins/page_rules/components/Condition';
+import {LayoutData} from '../../types/layout_data/LayoutData';
 
-	/**
-	 * Get users
-	 */
-	getUsers(): Promise<
-		Array<{
-			screenName: string;
-			userId: string;
-		}>
-	>;
+/**
+ * Add a rule
+ */
+declare type AddRuleProps = {
+	actions: Action[];
+	conditions: Condition[];
+	name: string;
+	onNetworkStatus: () => {};
+	segmentsExperienceId: string;
+};
+declare function addRule({
+	actions,
+	conditions,
+	name,
+	onNetworkStatus,
+	segmentsExperienceId,
+}: AddRuleProps): Promise<
+	Array<{
+		layoutData: LayoutData;
+	}>
+>;
+
+/**
+ * Delete a rule
+ */
+declare type DeleteRuleProps = {
+	onNetworkStatus: () => {};
+	ruleId: string;
+	segmentsExperienceId: string;
+};
+declare function deleteRule({
+	onNetworkStatus,
+	ruleId,
+	segmentsExperienceId,
+}: DeleteRuleProps): Promise<
+	Array<{
+		layoutData: LayoutData;
+	}>
+>;
+
+/**
+ * Get users
+ */
+declare function getUsers(): Promise<
+	Array<{
+		screenName: string;
+		userId: string;
+	}>
+>;
+
+/**
+ * Update a rule with new name, actions and conditions
+ */
+declare type UpdateRuleProps = {
+	actions: Action[];
+	conditions: Condition[];
+	name: string;
+	onNetworkStatus: () => {};
+	ruleId: string;
+	segmentsExperienceId: string;
+};
+declare function updateRule({
+	actions,
+	conditions,
+	name,
+	onNetworkStatus,
+	ruleId,
+	segmentsExperienceId,
+}: UpdateRuleProps): Promise<
+	Array<{
+		layoutData: LayoutData;
+	}>
+>;
+declare const _default: {
+	addRule: typeof addRule;
+	deleteRule: typeof deleteRule;
+	getUsers: typeof getUsers;
+	updateRule: typeof updateRule;
 };
 export default _default;
