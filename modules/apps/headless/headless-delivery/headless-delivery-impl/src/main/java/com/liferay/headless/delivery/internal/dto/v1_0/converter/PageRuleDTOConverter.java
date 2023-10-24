6c438e7ea22de5ff0820d8795f29fde057ede09c
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-10-24 10:49:14 +0200

    LPS-199722 Implements page rules export

---
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/converter/PageRuleDTOConverter.java
@@ -0,0 +1,40 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.delivery.internal.dto.v1_0.converter;
+
+import com.liferay.headless.delivery.dto.v1_0.PageRule;
+import com.liferay.headless.delivery.internal.dto.v1_0.mapper.LayoutStructureRuleMapper;
+import com.liferay.layout.util.structure.LayoutStructureRule;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(
+	property = "dto.class.name=com.liferay.layout.util.structure.LayoutStructureRule",
+	service = DTOConverter.class
+)
+public class PageRuleDTOConverter
+	implements DTOConverter<LayoutStructureRule, PageRule> {
+
+	@Override
+	public String getContentType() {
+		return PageRule.class.getSimpleName();
+	}
+
+	@Override
+	public PageRule toDTO(
+			DTOConverterContext dtoConverterContext,
+			LayoutStructureRule layoutStructureRule)
+		throws Exception {
+
+		return LayoutStructureRuleMapper.getPageRule(layoutStructureRule);
+	}
+
+}
---
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/util/PageRulesUtil.java
@@ -0,0 +1,36 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.delivery.internal.dto.v1_0.util;
+
+import com.liferay.headless.delivery.dto.v1_0.PageRule;
+import com.liferay.headless.delivery.internal.dto.v1_0.mapper.LayoutStructureRuleMapper;
+import com.liferay.layout.util.structure.LayoutStructureRule;
+import com.liferay.petra.function.transform.TransformUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+
+import java.util.List;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+public class PageRulesUtil {
+
+	public static PageRule[] toPageRules(
+		List<LayoutStructureRule> layoutStructureRules) {
+
+		if (ListUtil.isEmpty(layoutStructureRules)) {
+			return null;
+		}
+
+		List<PageRule> pageRules = TransformUtil.transform(
+			layoutStructureRules,
+			layoutStructureRule -> LayoutStructureRuleMapper.getPageRule(
+				layoutStructureRule));
+
+		return pageRules.toArray(new PageRule[0]);
+	}
+
+}
