2d8e0e11f9f715e81f4be11935cc1697f9488d9d
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-10-28 14:20:32 +0200

    LPS-200228 Adds a ModelListener for LayoutPageTemplateEntry to prevent the content type changes when the display page is used by at least one asset.
    I am using a model listener for this because trying to check it in the service layer will cause a circular dependency between AssetDisplayPageEntryLocalService and LayoutPageEntryLocalService. Checking it only in the action command would leave open the possibility to change the content type of a used display page by invoking directly the service method.

---
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/model/listener/LayoutPageTemplateEntryModelListener.java
@@ -0,0 +1,80 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.layout.page.template.admin.web.internal.model.listener;
+
+import com.liferay.asset.display.page.service.AssetDisplayPageEntryLocalService;
+import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
+import com.liferay.layout.page.template.exception.RequiredLayoutPageTemplateEntryException;
+import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
+
+import java.util.Objects;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(service = ModelListener.class)
+public class LayoutPageTemplateEntryModelListener
+	extends BaseModelListener<LayoutPageTemplateEntry> {
+
+	@Override
+	public void onAfterUpdate(
+			LayoutPageTemplateEntry originalLayoutPageTemplateEntry,
+			LayoutPageTemplateEntry layoutPageTemplateEntry)
+		throws ModelListenerException {
+
+		if (_isContentTypeChanged(
+				layoutPageTemplateEntry, originalLayoutPageTemplateEntry)) {
+
+			int usagesCount =
+				_assetDisplayPageEntryLocalService.
+					getAssetDisplayPageEntriesCountByLayoutPageTemplateEntryId(
+						layoutPageTemplateEntry.getLayoutPageTemplateEntryId());
+
+			if (usagesCount > 0) {
+				throw new ModelListenerException(
+					new RequiredLayoutPageTemplateEntryException());
+			}
+		}
+	}
+
+	private boolean _isContentTypeChanged(
+		LayoutPageTemplateEntry layoutPageTemplateEntry,
+		LayoutPageTemplateEntry originalLayoutPageTemplateEntry) {
+
+		if (!FeatureFlagManagerUtil.isEnabled("LPS-195263") ||
+			!Objects.equals(
+				layoutPageTemplateEntry.getType(),
+				LayoutPageTemplateEntryTypeConstants.DISPLAY_PAGE) ||
+			(originalLayoutPageTemplateEntry.getClassNameId() == 0)) {
+
+			return false;
+		}
+
+		if (!Objects.equals(
+				originalLayoutPageTemplateEntry.getClassNameId(),
+				layoutPageTemplateEntry.getClassNameId()) ||
+			!Objects.equals(
+				originalLayoutPageTemplateEntry.getClassTypeId(),
+				layoutPageTemplateEntry.getClassTypeId())) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Reference
+	private AssetDisplayPageEntryLocalService
+		_assetDisplayPageEntryLocalService;
+
+}
