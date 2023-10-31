7c3bf6b94f875eb9be6f6a8f2b7074102d5bad02
Sandro Chinea <sandrochinea@gmail.com>
2023-10-31 16:52:44 +0000

    LPS-200228 Not needed

--- modules/apps/layout/layout-page-template-admin-web/src/main/resources/META-INF/resources/js/modal/DisplayPageModal.tsx
+++ modules/apps/layout/layout-page-template-admin-web/src/main/resources/META-INF/resources/js/modal/DisplayPageModal.tsx
@@ -152,8 +152,6 @@ export default function DisplayPageModal({
 		[formSubmitURL, onClose, type, validateForm]
 	);
 
-	const visible = observer.mutation;
-
 	return (
 		<ClayModal observer={observer}>
 			<ClayModal.Header>{title}</ClayModal.Header>
@@ -180,17 +178,15 @@ export default function DisplayPageModal({
 					</ClayAlert>
 				)}
 
-				{visible && (
-					<DisplayPageModalForm
-						displayPageName={displayPageName}
-						error={error}
-						formRef={formRef}
-						mappingTypes={mappingTypes}
-						namespace={namespace}
-						onSubmit={handleSubmit}
-						type={type}
-					/>
-				)}
+				<DisplayPageModalForm
+					displayPageName={displayPageName}
+					error={error}
+					formRef={formRef}
+					mappingTypes={mappingTypes}
+					namespace={namespace}
+					onSubmit={handleSubmit}
+					type={type}
+				/>
 			</ClayModal.Body>
 
 			<ClayModal.Footer
