daa85208383c01f9deb45eaa85ce49382d4cc15e
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-12-05 16:39:13 +0100

    LPS-203213 SF moves the implementation to the suitable package

--- modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/configuration/manager/MenuAccessConfigurationManagerImpl.java
+++ modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/configuration/manager/MenuAccessConfigurationManagerImpl.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.site.internal.configuration;
+package com.liferay.site.internal.configuration.manager;
 
 import com.liferay.portal.configuration.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.model.Group;
