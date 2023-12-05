fee4778f27a05f1b1fe79996742eab59d5c96349
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-12-05 16:37:57 +0100

    LPS-203213 Adds an interface and its implementation to handle sitemap configuration

---
+++ modules/apps/site/site-api/src/main/java/com/liferay/site/configuration/manager/SitemapConfigurationManager.java
@@ -0,0 +1,23 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.configuration.manager;
+
+import com.liferay.portal.kernel.module.configuration.ConfigurationException;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+public interface SitemapConfigurationManager {
+
+	public boolean includeCategories(long companyId)
+		throws ConfigurationException;
+
+	public boolean includePages(long companyId) throws ConfigurationException;
+
+	public boolean includeWebContent(long companyId)
+		throws ConfigurationException;
+
+}
---
+++ modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/configuration/manager/SitemapConfigurationManagerImpl.java
@@ -0,0 +1,57 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.internal.configuration.manager;
+
+import com.liferay.portal.configuration.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.module.configuration.ConfigurationException;
+import com.liferay.site.configuration.manager.SitemapConfigurationManager;
+import com.liferay.site.internal.configuration.SitemapCompanyConfiguration;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(service = SitemapConfigurationManager.class)
+public class SitemapConfigurationManagerImpl
+	implements SitemapConfigurationManager {
+
+	@Override
+	public boolean includeCategories(long companyId)
+		throws ConfigurationException {
+
+		SitemapCompanyConfiguration sitemapCompanyConfiguration =
+			_configurationProvider.getCompanyConfiguration(
+				SitemapCompanyConfiguration.class, companyId);
+
+		return sitemapCompanyConfiguration.includeCategories();
+	}
+
+	@Override
+	public boolean includePages(long companyId) throws ConfigurationException {
+		SitemapCompanyConfiguration sitemapCompanyConfiguration =
+			_configurationProvider.getCompanyConfiguration(
+				SitemapCompanyConfiguration.class, companyId);
+
+		return sitemapCompanyConfiguration.includePages();
+	}
+
+	@Override
+	public boolean includeWebContent(long companyId)
+		throws ConfigurationException {
+
+		SitemapCompanyConfiguration sitemapCompanyConfiguration =
+			_configurationProvider.getCompanyConfiguration(
+				SitemapCompanyConfiguration.class, companyId);
+
+		return sitemapCompanyConfiguration.includeWebContent();
+	}
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+}
