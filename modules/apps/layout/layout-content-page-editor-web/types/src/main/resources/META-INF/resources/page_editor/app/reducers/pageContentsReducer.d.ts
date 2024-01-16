0efad620530ba4f5454951ff657e5cd8616ee9a2
Sandro Chinea <sandrochinea@gmail.com>
2024-01-16 11:43:00 +0000

    LPS-205412 Types

--- modules/apps/layout/layout-content-page-editor-web/types/src/main/resources/META-INF/resources/page_editor/app/reducers/pageContentsReducer.d.ts
+++ modules/apps/layout/layout-content-page-editor-web/types/src/main/resources/META-INF/resources/page_editor/app/reducers/pageContentsReducer.d.ts
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import addItem, {PageContent} from '../actions/addItem';
+import {PageContent} from '../../types/PageContent';
+import addItem from '../actions/addItem';
 import deleteItem from '../actions/deleteItem';
 import updateCollectionDisplayCollection from '../actions/updateCollectionDisplayCollection';
 import updateEditableValues from '../actions/updateEditableValues';
@@ -13,7 +14,7 @@ import updatePageContents from '../actions/updatePageContents';
 import updatePreviewImage from '../actions/updatePreviewImage';
 import updateRowColumns from '../actions/updateRowColumns';
 export default function pageContentsReducer(
-	pageContents: PageContent[] | undefined,
+	pageContents: PageContent[] | null | undefined,
 	action: ReturnType<
 		| typeof addItem
 		| typeof deleteItem
@@ -25,34 +26,36 @@ export default function pageContentsReducer(
 		| typeof updatePreviewImage
 		| typeof updateRowColumns
 	>
-): (
-	| PageContent
-	| {
-			actions: {
-				editImage: {
-					previewURL: string;
-					editImageURL?: string | undefined;
-					fileEntryId?: string | undefined;
-				};
-				editURL?: string | undefined;
-				permissionsURL?: string | undefined;
-				viewUsagesURL?: string | undefined;
-			};
-			className: string;
-			classNameId: string;
-			classPK: string;
-			classTypeId: string;
-			externalReferenceCode: string;
-			icon: string;
-			isRestricted: boolean;
-			status: {
-				hasApprovedVersion: boolean;
-				label: string;
-				style: string;
-			};
-			subtype: string;
-			title: string;
-			type: string;
-			usagesCount: number;
-	  }
-)[];
+):
+	| (
+			| PageContent
+			| {
+					actions: {
+						editImage: {
+							previewURL: string;
+							editImageURL?: string | undefined;
+							fileEntryId?: string | undefined;
+						};
+						editURL?: string | undefined;
+						permissionsURL?: string | undefined;
+						viewUsagesURL?: string | undefined;
+					};
+					className: string;
+					classNameId: string;
+					classPK: string;
+					classTypeId: string;
+					externalReferenceCode: string;
+					icon: string;
+					isRestricted: boolean;
+					status: {
+						hasApprovedVersion: boolean;
+						label: string;
+						style: string;
+					};
+					subtype: string;
+					title: string;
+					type: string;
+					usagesCount: number;
+			  }
+	  )[]
+	| null;
