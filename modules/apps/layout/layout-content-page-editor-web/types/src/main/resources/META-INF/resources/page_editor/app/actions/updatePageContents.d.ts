7c2c2000c2b145ae83262f089f20d4bc57cc7147
Sandro Chinea <sandrochinea@gmail.com>
2024-01-16 11:43:00 +0000

    LPS-205412 Types

--- modules/apps/layout/layout-content-page-editor-web/types/src/main/resources/META-INF/resources/page_editor/app/actions/updatePageContents.d.ts
+++ modules/apps/layout/layout-content-page-editor-web/types/src/main/resources/META-INF/resources/page_editor/app/actions/updatePageContents.d.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import type {PageContent} from './addItem';
+import {PageContent} from '../../types/PageContent';
 export default function updatePageContents({
 	pageContents,
 }: {
