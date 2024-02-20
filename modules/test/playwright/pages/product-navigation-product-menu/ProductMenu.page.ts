5f19cdd7feb62a04ad0e2f50cca8bd78aa7c6679
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-02-20 14:43:32 +0100

    LPD-15577 SF sort alphabetical but page, No logic changes

--- modules/test/playwright/pages/product-navigation-product-menu/ProductMenu.page.ts
+++ modules/test/playwright/pages/product-navigation-product-menu/ProductMenu.page.ts
@@ -6,34 +6,32 @@
 import {Locator, Page} from '@playwright/test';
 
 export class ProductMenuPage {
+	readonly page: Page;
+
 	readonly closeProductMenuButton: Locator;
 	readonly configurationMenuItem: Locator;
 	readonly contentAndDataMenuItem: Locator;
-	readonly knowledgeBaseMenuItem: Locator;
-	readonly journalMenuItem: Locator;
-	readonly lockedItemsMenuItem: Locator;
-	readonly openProductMenuButton: Locator;
-	readonly page: Page;
 	readonly documentsAndMediaMenuItem: Locator;
-	readonly siteBuilderMenuItem: Locator;
+	readonly journalMenuItem: Locator;
+	readonly knowledgeBaseMenuItem: Locator;
+	readonly openProductMenuButton: Locator;
 	readonly pagesMenuItem: Locator;
+	readonly siteBuilderMenuItem: Locator;
 
 	constructor(page: Page) {
+		this.page = page;
+
 		this.closeProductMenuButton = page.getByLabel('Close Product Menu');
 		this.configurationMenuItem = page.getByRole('menuitem', {
 			exact: true,
 			name: 'Configuration',
 		});
-		this.siteBuilderMenuItem = page.getByRole('menuitem', {
-			exact: true,
-			name: 'Site Builder',
-		});
-		this.pagesMenuItem = page.getByRole('menuitem', {
-			name: 'Pages',
-		});
 		this.contentAndDataMenuItem = page.getByRole('menuitem', {
 			exact: true,
 			name: 'Content & Data',
+		});
+		this.documentsAndMediaMenuItem = page.getByRole('menuitem', {
+			name: 'Documents and Media',
 		});
 		this.journalMenuItem = page.getByRole('menuitem', {
 			name: 'Web Content',
@@ -42,28 +40,14 @@ export class ProductMenuPage {
 			exact: true,
 			name: 'Knowledge Base',
 		});
-		this.lockedItemsMenuItem = page.getByRole('menuitem', {
-			exact: true,
-			name: 'Locked Items',
-		});
-		this.documentsAndMediaMenuItem = page.getByRole('menuitem', {
-			name: 'Documents and Media',
-		});
-
 		this.openProductMenuButton = page.getByLabel('Open Product Menu');
-		this.page = page;
-	}
-
-	async goto() {
-		await this.page.goto('/');
-	}
-
-	async openProductMenu() {
-		await this.goto();
-
-		if (await this.openProductMenuButton.isVisible()) {
-			await this.openProductMenuButton.click();
-		}
+		this.pagesMenuItem = page.getByRole('menuitem', {
+			name: 'Pages',
+		});
+		this.siteBuilderMenuItem = page.getByRole('menuitem', {
+			exact: true,
+			name: 'Site Builder',
+		});
 	}
 
 	async closeProductMenu() {
@@ -74,24 +58,8 @@ export class ProductMenuPage {
 		}
 	}
 
-	async goToJournalMenuItem() {
-		await this.goToContentAndData();
-		await this.journalMenuItem.click();
-	}
-
-	async goToKnowledgeBaseMenuItem() {
-		await this.goToContentAndData();
-		await this.knowledgeBaseMenuItem.click();
-	}
-
-	async goToLockedItemsMenuItem() {
-		await this.goToConfiguration();
-		await this.lockedItemsMenuItem.click();
-	}
-
-	async goToDocumentsAndMediaMenuItem() {
-		await this.goToContentAndData();
-		await this.documentsAndMediaMenuItem.click();
+	async goto() {
+		await this.page.goto('/');
 	}
 
 	async goToConfiguration() {
@@ -117,6 +85,21 @@ export class ProductMenuPage {
 		}
 	}
 
+	async goToDocumentsAndMediaMenuItem() {
+		await this.goToContentAndData();
+		await this.documentsAndMediaMenuItem.click();
+	}
+
+	async goToJournalMenuItem() {
+		await this.goToContentAndData();
+		await this.journalMenuItem.click();
+	}
+
+	async goToKnowledgeBaseMenuItem() {
+		await this.goToContentAndData();
+		await this.knowledgeBaseMenuItem.click();
+	}
+
 	async goToPagesMenuItem() {
 		await this.goToContentAndData();
 		await this.pagesMenuItem.click();
@@ -131,6 +114,14 @@ export class ProductMenuPage {
 
 		if (isClosed) {
 			await this.siteBuilderMenuItem.click();
+		}
+	}
+
+	async openProductMenu() {
+		await this.goto();
+
+		if (await this.openProductMenuButton.isVisible()) {
+			await this.openProductMenuButton.click();
 		}
 	}
 }
