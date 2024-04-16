bc61b29688909a542cc3a2ee79bf82c8a40e7959
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-04-16 15:39:30 +0200

    LPD-21974 Add support for the script field

--- modules/apps/layout/layout-api/src/main/java/com/liferay/layout/util/structure/LayoutStructure.java
+++ modules/apps/layout/layout-api/src/main/java/com/liferay/layout/util/structure/LayoutStructure.java
@@ -793,7 +793,7 @@ public class LayoutStructure {
 
 	public LayoutStructureRule updateLayoutStructureRule(
 		JSONArray actionsJSONArray, JSONArray conditionsJSONArray,
-		String conditionType, String name, String ruleId) {
+		String conditionType, String name, String script, String ruleId) {
 
 		LayoutStructureRule layoutStructureRule = _layoutStructureRulesMap.get(
 			ruleId);
@@ -803,6 +803,7 @@ public class LayoutStructure {
 			layoutStructureRule.setConditionsJSONArray(conditionsJSONArray);
 			layoutStructureRule.setConditionType(conditionType);
 			layoutStructureRule.setName(name);
+			layoutStructureRule.setScript(script);
 		}
 
 		return layoutStructureRule;
