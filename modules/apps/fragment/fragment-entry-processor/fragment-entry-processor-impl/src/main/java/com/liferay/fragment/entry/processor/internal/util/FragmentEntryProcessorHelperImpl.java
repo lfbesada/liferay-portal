c7312052897b472fa6c210c60275328a1aa51c87
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-04-17 16:24:31 +0200

    LPD-12882 Move the permission checking to apply also on getMappedInfoItemFieldValue invocations

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-impl/src/main/java/com/liferay/fragment/entry/processor/internal/util/FragmentEntryProcessorHelperImpl.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-impl/src/main/java/com/liferay/fragment/entry/processor/internal/util/FragmentEntryProcessorHelperImpl.java
@@ -108,29 +108,6 @@ public class FragmentEntryProcessorHelperImpl
 			return null;
 		}
 
-		if (!_hasViewPermission(
-				fragmentEntryProcessorContext.getHttpServletRequest(),
-				infoItemMappedField)) {
-
-			if (Objects.equals(
-					fragmentEntryProcessorContext.getMode(),
-					FragmentEntryLinkConstants.EDIT)) {
-
-				return StringBundler.concat(
-					"<span class=\"clearfix page-editor__editable\" ",
-					"data-lfr-editable-id=\"02-title\">",
-					_language.get(
-						fragmentEntryProcessorContext.getLocale(),
-						"restricted-content"),
-					"<svg class=\"lexicon-icon lexicon-icon-password-policies",
-					"\" role=\"presentation\" viewBox=\"0 0 512 512\">",
-					"<use xlink:href=\"/o/classic-theme/images/clay",
-					"/icons.svg#password-policies\"></use></svg></span>");
-			}
-
-			return StringPool.BLANK;
-		}
-
 		TrashHandler trashHandler = TrashHandlerRegistryUtil.getTrashHandler(
 			infoItemMappedField.getClassName());
 
@@ -265,6 +242,31 @@ public class FragmentEntryProcessorHelperImpl
 			return null;
 		}
 
+		InfoField infoField = infoFieldValue.getInfoField();
+
+		if (!_hasViewPermission(
+				fragmentEntryProcessorContext.getHttpServletRequest(),
+				infoItemFieldValues.getInfoItemReference())) {
+
+			if (!Objects.equals(
+					fragmentEntryProcessorContext.getMode(),
+					FragmentEntryLinkConstants.EDIT)) {
+
+				return StringPool.BLANK;
+			}
+
+			return StringBundler.concat(
+				"<span class=\"clearfix page-editor__editable\" ",
+				"data-lfr-editable-id=\"02-title\">",
+				_language.get(
+					fragmentEntryProcessorContext.getLocale(),
+					"restricted-content"),
+				"<svg class=\"lexicon-icon lexicon-icon-password-policies",
+				"\" role=\"presentation\" viewBox=\"0 0 512 512\">",
+				"<use xlink:href=\"/o/classic-theme/images/clay",
+				"/icons.svg#password-policies\"></use></svg></span>");
+		}
+
 		Object value = infoFieldValue.getValue(
 			fragmentEntryProcessorContext.getLocale());
 
@@ -312,8 +314,6 @@ public class FragmentEntryProcessorHelperImpl
 				fragmentEntryProcessorContext.getLocale());
 		}
 		else if (value instanceof String) {
-			InfoField infoField = infoFieldValue.getInfoField();
-
 			if (infoField.getInfoFieldType() instanceof DateInfoFieldType) {
 				Locale dateLocale = LocaleUtil.getSiteDefault();
 
@@ -428,7 +428,7 @@ public class FragmentEntryProcessorHelperImpl
 
 		return _hasViewPermission(
 			fragmentEntryProcessorContext.getHttpServletRequest(),
-			infoItemMappedField);
+			infoItemMappedField.getInfoItemReference());
 	}
 
 	@Override
@@ -769,7 +769,7 @@ public class FragmentEntryProcessorHelperImpl
 
 	private boolean _hasViewPermission(
 		HttpServletRequest httpServletRequest,
-		InfoItemMappedField infoItemMappedField) {
+		InfoItemReference infoItemReference) {
 
 		ThemeDisplay themeDisplay =
 			(ThemeDisplay)httpServletRequest.getAttribute(
@@ -779,12 +779,12 @@ public class FragmentEntryProcessorHelperImpl
 			InfoItemPermissionProvider infoItemPermissionProvider =
 				_infoItemServiceRegistry.getFirstInfoItemService(
 					InfoItemPermissionProvider.class,
-					infoItemMappedField.getClassName());
+					infoItemReference.getClassName());
 
 			if ((infoItemPermissionProvider != null) &&
 				!infoItemPermissionProvider.hasPermission(
-					themeDisplay.getPermissionChecker(),
-					infoItemMappedField.getObject(), ActionKeys.VIEW)) {
+					themeDisplay.getPermissionChecker(), infoItemReference,
+					ActionKeys.VIEW)) {
 
 				return false;
 			}
