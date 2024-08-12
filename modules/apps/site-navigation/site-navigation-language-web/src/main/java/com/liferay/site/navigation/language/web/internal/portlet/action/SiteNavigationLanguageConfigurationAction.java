e261c18916af9fa8d091208a8f2e5f16b31c017d
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-08-12 18:35:10 +0200

    LPD-33733 Persists or reset the displayStyleGroupExternalReferenceCode depending on the displayStyleGroupKey

--- modules/apps/site-navigation/site-navigation-language-web/src/main/java/com/liferay/site/navigation/language/web/internal/portlet/action/SiteNavigationLanguageConfigurationAction.java
+++ modules/apps/site-navigation/site-navigation-language-web/src/main/java/com/liferay/site/navigation/language/web/internal/portlet/action/SiteNavigationLanguageConfigurationAction.java
@@ -5,13 +5,23 @@
 
 package com.liferay.site.navigation.language.web.internal.portlet.action;
 
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.portlet.ConfigurationAction;
 import com.liferay.portal.kernel.portlet.DefaultConfigurationAction;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.site.navigation.language.constants.SiteNavigationLanguagePortletKeys;
+
+import javax.portlet.PortletPreferences;
+import javax.portlet.PortletRequest;
+import javax.portlet.ReadOnlyException;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Eudaldo Alonso
@@ -27,5 +37,38 @@ public class SiteNavigationLanguageConfigurationAction
 	public String getJspPath(HttpServletRequest httpServletRequest) {
 		return "/configuration.jsp";
 	}
+
+	@Override
+	protected void postProcess(
+		long companyId, PortletRequest portletRequest,
+		PortletPreferences portletPreferences) {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		Group group = _groupLocalService.fetchGroup(
+			themeDisplay.getCompanyId(),
+			getParameter(portletRequest, "displayStyleGroupKey"));
+
+		try {
+			if ((group != null) &&
+				(group.getGroupId() != themeDisplay.getScopeGroupId())) {
+
+				portletPreferences.setValue(
+					"displayStyleGroupExternalReferenceCode",
+					group.getExternalReferenceCode());
+			}
+			else {
+				portletPreferences.reset(
+					"displayStyleGroupExternalReferenceCode");
+			}
+		}
+		catch (ReadOnlyException readOnlyException) {
+			throw new SystemException(readOnlyException);
+		}
+	}
+
+	@Reference
+	private GroupLocalService _groupLocalService;
 
 }
