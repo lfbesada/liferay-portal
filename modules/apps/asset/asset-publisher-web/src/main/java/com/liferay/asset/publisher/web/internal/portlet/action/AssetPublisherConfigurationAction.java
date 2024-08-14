87b0c71933a90a44434e674d4da800574159e715
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-08-14 14:43:29 +0200

    LPD-30389 Set protected method for easy testing

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/portlet/action/AssetPublisherConfigurationAction.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/portlet/action/AssetPublisherConfigurationAction.java
@@ -245,7 +245,7 @@ public class AssetPublisherConfigurationAction
 							AssetPublisherSelectionStyleConstants.
 								TYPE_ASSET_LIST)) {
 
-					_updateAssetListEntryPreferences(
+					updateAssetListEntryPreferences(
 						actionRequest, portletPreferences);
 				}
 
@@ -333,6 +333,48 @@ public class AssetPublisherConfigurationAction
 	protected String getDefaultSelectionStyle() {
 		return AssetPublisherSelectionStyleConfigurationUtil.
 			defaultSelectionStyle();
+	}
+
+	protected void updateAssetListEntryPreferences(
+			ActionRequest actionRequest, PortletPreferences portletPreferences)
+		throws Exception {
+
+		if (!FeatureFlagManagerUtil.isEnabled("LPD-22837")) {
+			return;
+		}
+
+		AssetListEntry assetListEntry =
+			assetListEntryLocalService.fetchAssetListEntry(
+				GetterUtil.getLong(
+					getParameter(actionRequest, "assetListEntryId")));
+
+		if (assetListEntry == null) {
+			portletPreferences.reset("assetListEntryExternalReferenceCode");
+			portletPreferences.reset(
+				"assetListEntryGroupExternalReferenceCode");
+
+			return;
+		}
+
+		setPreference(
+			actionRequest, "assetListEntryExternalReferenceCode",
+			assetListEntry.getExternalReferenceCode());
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		if (assetListEntry.getGroupId() == themeDisplay.getScopeGroupId()) {
+			portletPreferences.reset(
+				"assetListEntryGroupExternalReferenceCode");
+		}
+		else {
+			Group group = groupLocalService.getGroup(
+				assetListEntry.getGroupId());
+
+			setPreference(
+				actionRequest, "assetListEntryGroupExternalReferenceCode",
+				group.getExternalReferenceCode());
+		}
 	}
 
 	@Reference
@@ -709,48 +751,6 @@ public class AssetPublisherConfigurationAction
 			displayStyle.equals("view-count-details")) {
 
 			portletPreferences.setValue("displayStyle", "full-content");
-		}
-	}
-
-	private void _updateAssetListEntryPreferences(
-			ActionRequest actionRequest, PortletPreferences portletPreferences)
-		throws Exception {
-
-		if (!FeatureFlagManagerUtil.isEnabled("LPD-22837")) {
-			return;
-		}
-
-		AssetListEntry assetListEntry =
-			assetListEntryLocalService.fetchAssetListEntry(
-				GetterUtil.getLong(
-					getParameter(actionRequest, "assetListEntryId")));
-
-		if (assetListEntry == null) {
-			portletPreferences.reset("assetListEntryExternalReferenceCode");
-			portletPreferences.reset(
-				"assetListEntryGroupExternalReferenceCode");
-
-			return;
-		}
-
-		setPreference(
-			actionRequest, "assetListEntryExternalReferenceCode",
-			assetListEntry.getExternalReferenceCode());
-
-		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		if (assetListEntry.getGroupId() == themeDisplay.getScopeGroupId()) {
-			portletPreferences.reset(
-				"assetListEntryGroupExternalReferenceCode");
-		}
-		else {
-			Group group = groupLocalService.getGroup(
-				assetListEntry.getGroupId());
-
-			setPreference(
-				actionRequest, "assetListEntryGroupExternalReferenceCode",
-				group.getExternalReferenceCode());
 		}
 	}
 
