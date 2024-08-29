a0ac87127d55881df0305b8767abe67facf52961
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-08-29 09:41:42 +0200

    LPD-34306 SF remove the feature flag

--- modules/apps/site-navigation/site-navigation-test/src/testIntegration/java/com/liferay/site/navigation/menu/web/internal/exportimport/test/SiteNavigationMenuExportImportTest.java
+++ modules/apps/site-navigation/site-navigation-test/src/testIntegration/java/com/liferay/site/navigation/menu/web/internal/exportimport/test/SiteNavigationMenuExportImportTest.java
@@ -36,7 +36,6 @@ import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.portal.kernel.util.UnicodeProperties;
-import com.liferay.portal.test.rule.FeatureFlags;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
@@ -63,7 +62,6 @@ import org.junit.runner.RunWith;
 /**
  * @author Javier Moral
  */
-@FeatureFlags("LPD-23048")
 @RunWith(Arquillian.class)
 public class SiteNavigationMenuExportImportTest
 	extends BasePortletExportImportTestCase {
