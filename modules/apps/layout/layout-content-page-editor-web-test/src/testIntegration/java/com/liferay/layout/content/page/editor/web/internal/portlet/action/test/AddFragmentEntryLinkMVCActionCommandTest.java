b8fb82003b44bf5becba7b8df55db88b2cf227f5
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-10-17 13:34:22 +0200

    LPD-37711 Add tests to assert that fragment with instanciable embedded portlet can be added multiple times using FreeMarker and also using tags

--- modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/test/AddFragmentEntryLinkMVCActionCommandTest.java
+++ modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/test/AddFragmentEntryLinkMVCActionCommandTest.java
@@ -14,15 +14,20 @@ import com.liferay.fragment.model.FragmentEntryLink;
 import com.liferay.fragment.service.FragmentCollectionLocalService;
 import com.liferay.fragment.service.FragmentEntryLinkLocalService;
 import com.liferay.fragment.service.FragmentEntryLocalService;
+import com.liferay.layout.content.page.editor.web.internal.portlet.constants.LayoutContentPageEditorWebPortletKeys;
 import com.liferay.layout.test.util.ContentLayoutTestUtil;
 import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.PortletPreferences;
+import com.liferay.portal.kernel.portlet.PortletIdCodec;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.PortletPreferencesLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionRequest;
@@ -35,6 +40,8 @@ import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.Inject;
@@ -103,6 +110,95 @@ public class AddFragmentEntryLinkMVCActionCommandTest {
 		}
 	}
 
+	@Test
+	public void testAddFragmentEntryLinkWithEmbeddedPortlet() throws Exception {
+		String instanceId = RandomTestUtil.randomString();
+
+		_assertAddFragmentEntryLinkWithEmbeddedPortlet(
+			_addFragmentEntry(
+				StringBundler.concat(
+					"<div><lfr-widget-",
+					LayoutContentPageEditorWebPortletKeys.
+						LAYOUT_CONTENT_PAGE_EDITOR_WEB_TEST_PORTLET_ALIAS,
+					" id=\"", instanceId, "\"></lfr-widget-",
+					LayoutContentPageEditorWebPortletKeys.
+						LAYOUT_CONTENT_PAGE_EDITOR_WEB_TEST_PORTLET_ALIAS,
+					"></div>")),
+			instanceId,
+			LayoutContentPageEditorWebPortletKeys.
+				LAYOUT_CONTENT_PAGE_EDITOR_WEB_TEST_PORTLET);
+		_assertAddFragmentEntryLinkWithEmbeddedPortlet(
+			_addFragmentEntry(
+				StringBundler.concat(
+					"<div>[@liferay_portlet.runtime portletName=\"",
+					LayoutContentPageEditorWebPortletKeys.
+						LAYOUT_CONTENT_PAGE_EDITOR_WEB_TEST_PORTLET,
+					"\" instanceId=\"${fragmentEntryLinkNamespace}", instanceId,
+					"\"/]/div>")),
+			instanceId,
+			LayoutContentPageEditorWebPortletKeys.
+				LAYOUT_CONTENT_PAGE_EDITOR_WEB_TEST_PORTLET);
+		_assertAddFragmentEntryLinkWithEmbeddedPortlet(
+			_addFragmentEntry(
+				StringBundler.concat(
+					"<div>[@liferay_portlet[\"runtime\"] portletName=\"",
+					LayoutContentPageEditorWebPortletKeys.
+						LAYOUT_CONTENT_PAGE_EDITOR_WEB_TEST_PORTLET,
+					"\" instanceId=\"${fragmentEntryLinkNamespace}", instanceId,
+					"\"/]/div>")),
+			instanceId,
+			LayoutContentPageEditorWebPortletKeys.
+				LAYOUT_CONTENT_PAGE_EDITOR_WEB_TEST_PORTLET);
+	}
+
+	private FragmentEntry _addFragmentEntry(String html) throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext();
+
+		FragmentCollection fragmentCollection =
+			_fragmentCollectionLocalService.addFragmentCollection(
+				null, TestPropsValues.getUserId(), _group.getGroupId(),
+				StringUtil.randomString(), StringPool.BLANK, serviceContext);
+
+		return _fragmentEntryLocalService.addFragmentEntry(
+			null, TestPropsValues.getUserId(), _group.getGroupId(),
+			fragmentCollection.getFragmentCollectionId(),
+			StringUtil.randomString(), StringUtil.randomString(),
+			RandomTestUtil.randomString(), html, RandomTestUtil.randomString(),
+			false, "{fieldSets: []}", null, 0, false,
+			FragmentConstants.TYPE_COMPONENT, null,
+			WorkflowConstants.STATUS_APPROVED, serviceContext);
+	}
+
+	private void _assertAddFragmentEntryLinkWithEmbeddedPortlet(
+			FragmentEntry fragmentEntry, String instanceId, String portletId)
+		throws Exception {
+
+		List<PortletPreferences> originalPortletPreferencesList =
+			_portletPreferencesLocalService.getPortletPreferencesByPlid(
+				_layout.getPlid());
+
+		FragmentEntryLink fragmentEntryLink = _testAddFragmentEntryLink(
+			fragmentEntry);
+
+		List<PortletPreferences> portletPreferencesList = ListUtil.remove(
+			_portletPreferencesLocalService.getPortletPreferencesByPlid(
+				_layout.getPlid()),
+			originalPortletPreferencesList);
+
+		Assert.assertEquals(
+			portletPreferencesList.toString(), 1,
+			portletPreferencesList.size());
+
+		PortletPreferences portletPreferences = portletPreferencesList.get(0);
+
+		Assert.assertEquals(
+			_portal.getJsSafePortletId(
+				PortletIdCodec.encode(
+					portletId, fragmentEntryLink.getNamespace() + instanceId)),
+			portletPreferences.getPortletId());
+	}
+
 	private FragmentEntry _getFragmentEntry(long groupId) throws Exception {
 		ServiceContext serviceContext =
 			ServiceContextTestUtil.getServiceContext();
@@ -136,7 +232,8 @@ public class AddFragmentEntryLinkMVCActionCommandTest {
 		return mockLiferayPortletActionRequest;
 	}
 
-	private void _testAddFragmentEntryLink(FragmentEntry fragmentEntry)
+	private FragmentEntryLink _testAddFragmentEntryLink(
+			FragmentEntry fragmentEntry)
 		throws Exception {
 
 		List<FragmentEntryLink> originalFragmentEntryLinks =
@@ -188,6 +285,8 @@ public class AddFragmentEntryLinkMVCActionCommandTest {
 			actualFragmentEntryLinks.toString(),
 			originalFragmentEntryLinks.size() + 1,
 			actualFragmentEntryLinks.size());
+
+		return fragmentEntryLink;
 	}
 
 	private Company _company;
@@ -213,5 +312,11 @@ public class AddFragmentEntryLinkMVCActionCommandTest {
 		filter = "mvc.command.name=/layout_content_page_editor/add_fragment_entry_link"
 	)
 	private MVCActionCommand _mvcActionCommand;
+
+	@Inject
+	private Portal _portal;
+
+	@Inject
+	private PortletPreferencesLocalService _portletPreferencesLocalService;
 
 }
