3c0a2c13793afb0ab05824091d0b894b2e3a67a6
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-10-29 09:09:10 +0100

    LPD-39121 Use the new deprecation feature flag

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/visibility/LayoutVisibilityManagerImpl.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/visibility/LayoutVisibilityManagerImpl.java
@@ -6,8 +6,7 @@
 package com.liferay.layout.internal.visibility;
 
 import com.liferay.layout.admin.kernel.visibility.LayoutVisibilityManager;
-import com.liferay.release.feature.flag.ReleaseFeatureFlag;
-import com.liferay.release.feature.flag.ReleaseFeatureFlagManagerUtil;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -19,13 +18,11 @@ public class LayoutVisibilityManagerImpl implements LayoutVisibilityManager {
 
 	@Override
 	public boolean isPrivateLayoutsEnabled(long companyId) {
-		if (ReleaseFeatureFlagManagerUtil.isEnabled(
-				ReleaseFeatureFlag.DISABLE_PRIVATE_LAYOUTS)) {
-
-			return false;
+		if (FeatureFlagManagerUtil.isEnabled(companyId, "LPD-38869")) {
+			return true;
 		}
 
-		return true;
+		return false;
 	}
 
 }
