09fdc2a78de0f4299d233c4acb588d91a017c45a
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-11-11 15:50:49 +0100

    LPD-41541 Add tests to assert GET and POST endpoints

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/PageTemplateResourceTest.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/PageTemplateResourceTest.java
@@ -6,14 +6,58 @@
 package com.liferay.headless.admin.site.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.headless.admin.site.client.dto.v1_0.PageTemplate;
+import com.liferay.portal.test.rule.FeatureFlags;
 
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 /**
  * @author Rubén Pulido
  */
-@Ignore
+@FeatureFlags("LPD-35443")
 @RunWith(Arquillian.class)
 public class PageTemplateResourceTest extends BasePageTemplateResourceTestCase {
+
+	@Override
+	protected String[] getAdditionalAssertFieldNames() {
+		return new String[] {"externalReferenceCode", "name"};
+	}
+
+	@Override
+	protected PageTemplate
+			testGetSiteSiteByExternalReferenceCodePageTemplatesPage_addPageTemplate(
+				String siteExternalReferenceCode, PageTemplate pageTemplate)
+		throws Exception {
+
+		return pageTemplateResource.
+			postSiteSiteByExternalReferenceCodePageTemplate(
+				siteExternalReferenceCode, pageTemplate);
+	}
+
+	@Override
+	protected String
+			testGetSiteSiteByExternalReferenceCodePageTemplatesPage_getIrrelevantSiteExternalReferenceCode()
+		throws Exception {
+
+		return irrelevantGroup.getExternalReferenceCode();
+	}
+
+	@Override
+	protected String
+			testGetSiteSiteByExternalReferenceCodePageTemplatesPage_getSiteExternalReferenceCode()
+		throws Exception {
+
+		return testGroup.getExternalReferenceCode();
+	}
+
+	@Override
+	protected PageTemplate
+			testPostSiteSiteByExternalReferenceCodePageTemplate_addPageTemplate(
+				PageTemplate pageTemplate)
+		throws Exception {
+
+		return testGetSiteSiteByExternalReferenceCodePageTemplatesPage_addPageTemplate(
+			testGroup.getExternalReferenceCode(), pageTemplate);
+	}
+
 }
