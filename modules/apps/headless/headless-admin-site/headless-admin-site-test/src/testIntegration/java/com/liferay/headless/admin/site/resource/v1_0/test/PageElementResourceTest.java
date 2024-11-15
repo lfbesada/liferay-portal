24b4de5b1d8f6a2d85e0bb7a5bbb1a2ceff6adfa
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-11-15 07:49:13 +0000

    LPD-41555 Ignore not implemented tests

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/PageElementResourceTest.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/PageElementResourceTest.java
@@ -6,14 +6,93 @@
 package com.liferay.headless.admin.site.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.test.rule.FeatureFlags;
 
 import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Rubén Pulido
  */
-@Ignore
+@FeatureFlags("LPD-35443")
 @RunWith(Arquillian.class)
 public class PageElementResourceTest extends BasePageElementResourceTestCase {
+
+	@Ignore
+	@Override
+	@Test
+	public void testDeleteSiteSiteByExternalReferenceCodePageElement()
+		throws Exception {
+
+		super.testDeleteSiteSiteByExternalReferenceCodePageElement();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGetSiteSiteByExternalReferenceCodePageElement()
+		throws Exception {
+
+		super.testGetSiteSiteByExternalReferenceCodePageElement();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGetSiteSiteByExternalReferenceCodePageElementPageElementsPage()
+		throws Exception {
+
+		super.
+			testGetSiteSiteByExternalReferenceCodePageElementPageElementsPage();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGetSiteSiteByExternalReferenceCodePageExperiencePageElementsPage()
+		throws Exception {
+
+		super.
+			testGetSiteSiteByExternalReferenceCodePageExperiencePageElementsPage();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testPatchSiteSiteByExternalReferenceCodePageElement()
+		throws Exception {
+
+		super.testPatchSiteSiteByExternalReferenceCodePageElement();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testPostSiteSiteByExternalReferenceCodePageElementFragmentComposition()
+		throws Exception {
+
+		super.
+			testPostSiteSiteByExternalReferenceCodePageElementFragmentComposition();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testPostSiteSiteByExternalReferenceCodePageExperiencePageElement()
+		throws Exception {
+
+		super.
+			testPostSiteSiteByExternalReferenceCodePageExperiencePageElement();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testPutSiteSiteByExternalReferenceCodePageElement()
+		throws Exception {
+
+		super.testPutSiteSiteByExternalReferenceCodePageElement();
+	}
+
 }
