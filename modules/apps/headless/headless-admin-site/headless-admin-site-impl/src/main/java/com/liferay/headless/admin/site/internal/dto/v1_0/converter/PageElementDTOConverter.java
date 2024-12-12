39bca066aa85a9abbcfbce8ab77e8174c8128980
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-12-12 22:48:06 +0100

    LPD-44292 Uses dto context LayoutStructure to set position and page elements

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/converter/PageElementDTOConverter.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/converter/PageElementDTOConverter.java
@@ -26,11 +26,14 @@ import com.liferay.layout.util.structure.FormStepContainerStyledLayoutStructureI
 import com.liferay.layout.util.structure.FormStyledLayoutStructureItem;
 import com.liferay.layout.util.structure.FragmentDropZoneLayoutStructureItem;
 import com.liferay.layout.util.structure.FragmentStyledLayoutStructureItem;
+import com.liferay.layout.util.structure.LayoutStructure;
 import com.liferay.layout.util.structure.LayoutStructureItem;
 import com.liferay.layout.util.structure.RowStyledLayoutStructureItem;
+import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
 
+import java.util.List;
 import java.util.Objects;
 
 import org.osgi.service.component.annotations.Component;
@@ -57,14 +60,40 @@ public class PageElementDTOConverter
 			LayoutStructureItem layoutStructureItem)
 		throws Exception {
 
+		if (dtoConverterContext == null) {
+			throw new UnsupportedOperationException();
+		}
+
+		LayoutStructure layoutStructure =
+			(LayoutStructure)dtoConverterContext.getAttribute(
+				LayoutStructure.class.getName());
+
+		if (layoutStructure == null) {
+			throw new UnsupportedOperationException();
+		}
+
 		return new PageElement() {
 			{
 				setDefinition(() -> _getDefinition(layoutStructureItem));
 				setExternalReferenceCode(layoutStructureItem::getItemId);
-				setPageElements(() -> new PageElement[0]);
+				setPageElements(
+					() -> _getPageElements(
+						dtoConverterContext, layoutStructure,
+						layoutStructureItem));
 				setParentExternalReferenceCode(
 					layoutStructureItem::getParentItemId);
-				setPosition(() -> 0);
+				setPosition(
+					() -> {
+						LayoutStructureItem parentLayoutStructureItem =
+							layoutStructure.getLayoutStructureItem(
+								layoutStructureItem.getParentItemId());
+
+						List<String> childrenItemIds =
+							parentLayoutStructureItem.getChildrenItemIds();
+
+						return childrenItemIds.indexOf(
+							layoutStructureItem.getItemId());
+					});
 				setType(
 					() -> PageElementTypeUtil.toExternalType(
 						layoutStructureItem.getItemType()));
@@ -169,6 +198,19 @@ public class PageElementDTOConverter
 		}
 
 		throw new UnsupportedOperationException();
+	}
+
+	private PageElement[] _getPageElements(
+		DTOConverterContext dtoConverterContext,
+		LayoutStructure layoutStructure,
+		LayoutStructureItem layoutStructureItem) {
+
+		return TransformUtil.transformToArray(
+			layoutStructureItem.getChildrenItemIds(),
+			childrenItemId -> toDTO(
+				dtoConverterContext,
+				layoutStructure.getLayoutStructureItem(childrenItemId)),
+			PageElement.class);
 	}
 
 	@Reference(
