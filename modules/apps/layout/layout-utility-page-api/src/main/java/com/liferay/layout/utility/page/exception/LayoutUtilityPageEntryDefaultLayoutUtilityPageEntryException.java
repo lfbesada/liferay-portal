17c62ea2efbb3be51b0ecd1267185896672fcc9b
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-12-23 14:53:16 +0100

    LPD-42587 BuildService

---
+++ modules/apps/layout/layout-utility-page-api/src/main/java/com/liferay/layout/utility/page/exception/LayoutUtilityPageEntryDefaultLayoutUtilityPageEntryException.java
@@ -0,0 +1,37 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.layout.utility.page.exception;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class LayoutUtilityPageEntryDefaultLayoutUtilityPageEntryException
+	extends PortalException {
+
+	public LayoutUtilityPageEntryDefaultLayoutUtilityPageEntryException() {
+	}
+
+	public LayoutUtilityPageEntryDefaultLayoutUtilityPageEntryException(
+		String msg) {
+
+		super(msg);
+	}
+
+	public LayoutUtilityPageEntryDefaultLayoutUtilityPageEntryException(
+		String msg, Throwable throwable) {
+
+		super(msg, throwable);
+	}
+
+	public LayoutUtilityPageEntryDefaultLayoutUtilityPageEntryException(
+		Throwable throwable) {
+
+		super(throwable);
+	}
+
+}
