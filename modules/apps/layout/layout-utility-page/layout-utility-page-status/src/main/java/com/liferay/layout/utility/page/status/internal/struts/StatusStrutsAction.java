c90603c82a14636db1a11b13cf7d109a2df0f991
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-12-30 11:55:21 +0100

    LPD-1226 Add a warning trace

--- modules/apps/layout/layout-utility-page/layout-utility-page-status/src/main/java/com/liferay/layout/utility/page/status/internal/struts/StatusStrutsAction.java
+++ modules/apps/layout/layout-utility-page/layout-utility-page-status/src/main/java/com/liferay/layout/utility/page/status/internal/struts/StatusStrutsAction.java
@@ -6,6 +6,8 @@
 package com.liferay.layout.utility.page.status.internal.struts;
 
 import com.liferay.petra.io.unsync.UnsyncStringWriter;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.service.LayoutSetLocalService;
 import com.liferay.portal.kernel.servlet.PipingServletResponse;
@@ -79,6 +81,12 @@ public class StatusStrutsAction implements StrutsAction {
 		Element element = document.getElementById("content");
 
 		if (element == null) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"Theme " + layoutSet.getThemeId() +
+						" lacks a 'content' ID, replacing all body content");
+			}
+
 			element = document.body();
 		}
 
@@ -88,6 +96,9 @@ public class StatusStrutsAction implements StrutsAction {
 
 		return null;
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		StatusStrutsAction.class);
 
 	@Reference
 	private LayoutSetLocalService _layoutSetLocalService;
