0cead41fdc10aef0e5e078216248528768e464cd
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-02-13 14:41:30 +0100

    LPD-48779 Add information about layout page template collection type to the exception

--- modules/apps/layout/layout-page-template-api/src/main/java/com/liferay/layout/page/template/exception/LayoutPageTemplateCollectionLayoutPageTemplateCollectionKeyException.java
+++ modules/apps/layout/layout-page-template-api/src/main/java/com/liferay/layout/page/template/exception/LayoutPageTemplateCollectionLayoutPageTemplateCollectionKeyException.java
@@ -5,6 +5,7 @@
 
 package com.liferay.layout.page.template.exception;
 
+import com.liferay.layout.page.template.constants.LayoutPageTemplateCollectionTypeConstants;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.exception.PortalException;
 
@@ -15,17 +16,23 @@ public class
 	LayoutPageTemplateCollectionLayoutPageTemplateCollectionKeyException
 		extends PortalException {
 
+	public int getLayoutPageTemplateCollectionType() {
+		return _layoutPageTemplateCollectionType;
+	}
+
 	public static class MustNotBeDuplicate
 		extends LayoutPageTemplateCollectionLayoutPageTemplateCollectionKeyException {
 
 		public MustNotBeDuplicate(
-			long groupId, String layoutPageTemplateCollectionKey) {
+			long groupId, String layoutPageTemplateCollectionKey,
+			int layoutPageTemplateCollectionType) {
 
 			super(
 				StringBundler.concat(
-					"Duplicate layout page template collection for group ", groupId,
-					" with layout page template collection key ",
-					layoutPageTemplateCollectionKey));
+					"Duplicate layout page template collection for group ",
+					groupId, " with layout page template collection key ",
+					layoutPageTemplateCollectionKey),
+				layoutPageTemplateCollectionType);
 		}
 
 	}
@@ -34,14 +41,16 @@ public class
 		extends LayoutPageTemplateCollectionLayoutPageTemplateCollectionKeyException {
 
 		public MustNotContainInvalidCharacters(
-			String layoutPageTemplateCollectionKey) {
+			String layoutPageTemplateCollectionKey,
+			int layoutPageTemplateCollectionType) {
 
 			super(
 				StringBundler.concat(
 					"Layout page template collection key ",
 					layoutPageTemplateCollectionKey,
 					" must contain only alphanumeric characters, dashes, and ",
-					"underscores"));
+					"underscores"),
+				layoutPageTemplateCollectionType);
 		}
 
 	}
@@ -51,36 +60,59 @@ public class
 
 		public MustNotExceedMaximumSize(
 			String layoutPageTemplateCollectionKey,
-			int layoutPageTemplateCollectionKeyMaxSize) {
+			int layoutPageTemplateCollectionKeyMaxSize,
+			int layoutPageTemplateCollectionType) {
 
 			super(
 				StringBundler.concat(
 					"Layout page template collection key ",
 					layoutPageTemplateCollectionKey, " must have fewer than ",
-					layoutPageTemplateCollectionKeyMaxSize, " characters"));
+					layoutPageTemplateCollectionKeyMaxSize, " characters"),
+				layoutPageTemplateCollectionType);
 		}
 
 	}
 
 	private LayoutPageTemplateCollectionLayoutPageTemplateCollectionKeyException() {
+		_layoutPageTemplateCollectionType =
+			LayoutPageTemplateCollectionTypeConstants.BASIC;
 	}
 
 	private LayoutPageTemplateCollectionLayoutPageTemplateCollectionKeyException(
 		String msg) {
 
 		super(msg);
+
+		_layoutPageTemplateCollectionType =
+			LayoutPageTemplateCollectionTypeConstants.BASIC;
+	}
+
+	private LayoutPageTemplateCollectionLayoutPageTemplateCollectionKeyException(
+		String msg, int layoutPageTemplateCollectionType) {
+
+		super(msg);
+
+		_layoutPageTemplateCollectionType = layoutPageTemplateCollectionType;
 	}
 
 	private LayoutPageTemplateCollectionLayoutPageTemplateCollectionKeyException(
 		String msg, Throwable throwable) {
 
 		super(msg, throwable);
+
+		_layoutPageTemplateCollectionType =
+			LayoutPageTemplateCollectionTypeConstants.BASIC;
 	}
 
 	private LayoutPageTemplateCollectionLayoutPageTemplateCollectionKeyException(
 		Throwable throwable) {
 
 		super(throwable);
+
+		_layoutPageTemplateCollectionType =
+			LayoutPageTemplateCollectionTypeConstants.BASIC;
 	}
+
+	private final int _layoutPageTemplateCollectionType;
 
 }
