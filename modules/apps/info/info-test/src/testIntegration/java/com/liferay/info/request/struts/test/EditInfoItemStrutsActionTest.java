bf83e049c059dc46c2f6f68faffb0d08d54e40f3
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-03-24 07:04:11 +0100

    LPD-50374 Prevent a validation error during set up when the feature flag is enabled

--- modules/apps/info/info-test/src/testIntegration/java/com/liferay/info/request/struts/test/EditInfoItemStrutsActionTest.java
+++ modules/apps/info/info-test/src/testIntegration/java/com/liferay/info/request/struts/test/EditInfoItemStrutsActionTest.java
@@ -861,10 +861,18 @@ public class EditInfoItemStrutsActionTest {
 				ObjectFieldConstants.DB_TYPE_STRING,
 				RandomTestUtil.randomString(), "myRichText", false));
 
+		boolean enabledLocalization = false;
+
+		if (FeatureFlagManagerUtil.isEnabled(
+				TestPropsValues.getCompanyId(), "LPD-21926")) {
+
+			enabledLocalization = true;
+		}
+
 		ObjectDefinition objectDefinition =
 			_objectDefinitionLocalService.addCustomObjectDefinition(
 				_user.getUserId(), 0, null, false,
-				enableFriendlyURLCustomization, true, false, true,
+				enableFriendlyURLCustomization, true, enabledLocalization, true,
 				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
 				ObjectDefinitionTestUtil.getRandomName(), null,
 				"control_panel.sites",
