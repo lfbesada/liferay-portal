733b176ad3a7083b5e0dc8644ef6adb736dd4f08
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-08-04 12:42:23 +0200

    LPD-59858 Restore the testing of assigning no categories, otherwise we aren't testing the case of unassigning all the categories

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/util/AssetTestUtil.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/util/AssetTestUtil.java
@@ -134,6 +134,10 @@ public class AssetTestUtil {
 
 		List<AssetCategory> assetCategories = new ArrayList<>();
 
+		if (RandomTestUtil.randomBoolean()) {
+			return assetCategories;
+		}
+
 		for (int i = 0; i < RandomTestUtil.randomInt(1, 3); i++) {
 			AssetVocabulary assetVocabulary =
 				AssetVocabularyLocalServiceUtil.addVocabulary(
