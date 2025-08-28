b6b6ae88a35faa2886542642068358fcee631003
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-08-28 14:44:49 +0200

    LPD-64053 BuildService

--- portal-impl/src/com/liferay/portal/service/persistence/impl/LayoutPersistenceImpl.java
+++ portal-impl/src/com/liferay/portal/service/persistence/impl/LayoutPersistenceImpl.java
@@ -21839,7 +21839,7 @@ public class LayoutPersistenceImpl
 		ctMergeColumnNames.add("iconImageId");
 		ctMergeColumnNames.add("themeId");
 		ctMergeColumnNames.add("colorSchemeId");
-		ctMergeColumnNames.add("styleBookEntryId");
+		ctMergeColumnNames.add("styleBookEntryERC");
 		ctMergeColumnNames.add("css");
 		ctMergeColumnNames.add("priority");
 		ctMergeColumnNames.add("faviconFileEntryId");
