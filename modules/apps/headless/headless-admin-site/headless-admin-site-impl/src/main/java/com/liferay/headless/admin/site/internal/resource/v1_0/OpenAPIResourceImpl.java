d2a9f7d3fd2f93a6eb36a6e06646efae0f07ac9e
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-10-24 07:19:06 +0200

    Revert "LPD-68132 gw buildREST"
    This reverts commit 808f19081d385bfcdf86cf3b048b70d2231a9a3a.

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/OpenAPIResourceImpl.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/OpenAPIResourceImpl.java
@@ -95,8 +95,6 @@ public class OpenAPIResourceImpl {
 
 			add(MasterPageResourceImpl.class);
 
-			add(NavigationMenuResourceImpl.class);
-
 			add(PageElementResourceImpl.class);
 
 			add(PageExperienceResourceImpl.class);
