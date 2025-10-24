ee776d2c9bdcd8b0951dda33e83bf82d1c0d94ab
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-10-24 18:12:28 +0200

    LPD-XXX Add a util to access in static way to the fragment renderer registry

--- modules/apps/fragment/fragment-api/src/main/java/com/liferay/fragment/renderer/util/FragmentRendererRegistryUtil.java
+++ modules/apps/fragment/fragment-api/src/main/java/com/liferay/fragment/renderer/util/FragmentRendererRegistryUtil.java
@@ -3,7 +3,6 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-
 package com.liferay.fragment.renderer.util;
 
 import com.liferay.fragment.renderer.FragmentRenderer;
@@ -18,30 +17,32 @@ import java.util.List;
 public class FragmentRendererRegistryUtil {
 
 	public static FragmentRenderer getFragmentRenderer(String key) {
-		FragmentRendererRegistry fragmentRendererRegistry = _fragmentRendererRegistrySnapshot.get();
+		FragmentRendererRegistry fragmentRendererRegistry =
+			_fragmentRendererRegistrySnapshot.get();
 
 		return fragmentRendererRegistry.getFragmentRenderer(key);
 	}
 
+	public static FragmentRendererRegistry getFragmentRendererRegistry() {
+		return _fragmentRendererRegistrySnapshot.get();
+	}
+
 	public static List<FragmentRenderer> getFragmentRenderers() {
-		FragmentRendererRegistry fragmentRendererRegistry = _fragmentRendererRegistrySnapshot.get();
+		FragmentRendererRegistry fragmentRendererRegistry =
+			_fragmentRendererRegistrySnapshot.get();
 
 		return fragmentRendererRegistry.getFragmentRenderers();
 	}
 
 	public static List<FragmentRenderer> getFragmentRenderers(int type) {
-		FragmentRendererRegistry fragmentRendererRegistry = _fragmentRendererRegistrySnapshot.get();
+		FragmentRendererRegistry fragmentRendererRegistry =
+			_fragmentRendererRegistrySnapshot.get();
 
 		return fragmentRendererRegistry.getFragmentRenderers(type);
 	}
 
-	public static FragmentRendererRegistry
-	getFragmentRendererRegistry() {
-
-		return _fragmentRendererRegistrySnapshot.get();
-	}
 	private static final Snapshot<FragmentRendererRegistry>
 		_fragmentRendererRegistrySnapshot = new Snapshot<>(
-		FragmentRendererRegistryUtil.class,
-		FragmentRendererRegistry.class);
+			FragmentRendererRegistryUtil.class, FragmentRendererRegistry.class);
+
 }
