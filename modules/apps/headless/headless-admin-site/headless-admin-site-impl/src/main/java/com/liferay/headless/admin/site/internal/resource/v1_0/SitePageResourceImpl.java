b8012f1d86d028b3cf638b287cda6e6a5d5e5ee4
Carlos Correa <carlos.correa@liferay.com>
2025-11-04 17:58:39 +0100

    LPD-70456 Set pageSpecifications as nested fields to be exported along with the SitePage. The Batch Engine does not support the nested fields annotated with NestedFields yet, so we could use the factory here to call the same code in the end

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/SitePageResourceImpl.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/SitePageResourceImpl.java
@@ -81,8 +81,10 @@ import jakarta.ws.rs.core.MultivaluedMap;
 
 import java.io.Serializable;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
@@ -145,6 +147,11 @@ public class SitePageResourceImpl
 			@Override
 			public String getModelClassName() {
 				return Layout.class.getName();
+			}
+
+			@Override
+			public List<String> getNestedFields() {
+				return Arrays.asList("pageSpecifications");
 			}
 
 			@Override
