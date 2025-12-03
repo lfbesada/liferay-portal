9bff8e1aa59b579e13a38f97e1b984e51542504a
Georgel Pop <georgel.pop@liferay.com>
2025-12-03 10:03:34 +0100

    LPD-71877 Add a test case for testCopySegmentsExperienceData

--- modules/apps/layout/layout-content-page-editor-web/src/test/java/com/liferay/layout/content/page/editor/web/internal/segments/SegmentsExperienceUtilTest.java
+++ modules/apps/layout/layout-content-page-editor-web/src/test/java/com/liferay/layout/content/page/editor/web/internal/segments/SegmentsExperienceUtilTest.java
@@ -5,20 +5,52 @@
 
 package com.liferay.layout.content.page.editor.web.internal.segments;
 
+import com.liferay.counter.kernel.service.CounterLocalServiceUtil;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.processor.PortletRegistry;
+import com.liferay.fragment.service.FragmentEntryLinkLocalServiceUtil;
+import com.liferay.layout.content.page.editor.web.internal.util.layout.structure.LayoutStructureUtil;
+import com.liferay.layout.page.template.model.LayoutPageTemplateStructure;
+import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalServiceUtil;
+import com.liferay.layout.util.structure.FragmentStyledLayoutStructureItem;
+import com.liferay.layout.util.structure.LayoutStructure;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.comment.CommentManager;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.PortletPreferences;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.ResourcePermission;
+import com.liferay.portal.kernel.portlet.PortletIdCodec;
+import com.liferay.portal.kernel.service.PortletPreferencesLocalService;
+import com.liferay.portal.kernel.service.PortletPreferencesLocalServiceUtil;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalServiceUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.permission.PortletPermissionUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.language.LanguageImpl;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 import com.liferay.segments.model.SegmentsExperience;
+import com.liferay.segments.service.SegmentsExperimentLocalService;
+import com.liferay.segments.service.SegmentsExperimentLocalServiceUtil;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
+import org.mockito.stubbing.Answer;
 
 /**
  * @author David Arques
@@ -35,6 +67,385 @@ public class SegmentsExperienceUtilTest {
 		LanguageUtil languageUtil = new LanguageUtil();
 
 		languageUtil.setLanguage(new LanguageImpl());
+	}
+
+	@Before
+	public void setUp() {
+		SegmentsExperimentLocalService segmentsExperimentLocalService =
+			Mockito.mock(SegmentsExperimentLocalService.class);
+
+		_segmentsExperimentLocalServiceUtilMockedStatic = Mockito.mockStatic(
+			SegmentsExperimentLocalServiceUtil.class);
+
+		_segmentsExperimentLocalServiceUtilMockedStatic.when(
+			SegmentsExperimentLocalServiceUtil::getService
+		).thenReturn(
+			segmentsExperimentLocalService
+		);
+
+		Mockito.when(
+			segmentsExperimentLocalService.fetchSegmentsExperiment(
+				Mockito.anyLong(), Mockito.anyString(), Mockito.anyLong())
+		).thenReturn(
+			null
+		);
+
+		PortletPreferencesLocalService portletPreferencesLocalService =
+			Mockito.mock(PortletPreferencesLocalService.class);
+
+		_portletPreferencesLocalServiceUtilMockedStatic = Mockito.mockStatic(
+			PortletPreferencesLocalServiceUtil.class);
+
+		_portletPreferencesLocalServiceUtilMockedStatic.when(
+			PortletPreferencesLocalServiceUtil::getService
+		).thenReturn(
+			portletPreferencesLocalService
+		);
+
+		PortletPreferences mockPortletPreferences = Mockito.mock(
+			PortletPreferences.class);
+
+		Mockito.when(
+			portletPreferencesLocalService.fetchPortletPreferences(
+				Mockito.anyLong(), Mockito.anyInt(), Mockito.anyLong(),
+				Mockito.anyString())
+		).thenReturn(
+			mockPortletPreferences
+		);
+	}
+
+	@After
+	public void tearDown() {
+		_segmentsExperimentLocalServiceUtilMockedStatic.close();
+		_portletPreferencesLocalServiceUtilMockedStatic.close();
+	}
+
+	@Test
+	public void testCopySegmentsExperienceData() throws Exception {
+		long companyId = RandomTestUtil.randomLong();
+		long copiedResourcePermissionId = RandomTestUtil.randomLong();
+		long groupId = RandomTestUtil.randomLong();
+		long plid = RandomTestUtil.randomLong();
+		long resourceActionIds = RandomTestUtil.randomLong();
+		long roleId = RandomTestUtil.randomLong();
+		long sourceFragmentEntryLinkId = RandomTestUtil.randomLong();
+		long sourceSegmentsExperienceId = RandomTestUtil.randomLong();
+		long targetSegmentsExperienceId = RandomTestUtil.randomLong();
+		long userId = RandomTestUtil.randomLong();
+
+		String newNamespace = RandomTestUtil.randomString();
+		String oldNamespace = RandomTestUtil.randomString();
+
+		String portletId = RandomTestUtil.randomString() + "_INSTANCE_";
+
+		String sourcePortletId = portletId + oldNamespace;
+
+		String sourcePrimaryKey = RandomTestUtil.randomString();
+		String sourceSegmentsExperienceKey = RandomTestUtil.randomString();
+
+		String targetPortletId = portletId + newNamespace;
+
+		String targetPrimaryKey = RandomTestUtil.randomString();
+
+		CommentManager commentManager = Mockito.mock(CommentManager.class);
+		PortletRegistry portletRegistry = Mockito.mock(PortletRegistry.class);
+
+		Layout layout = Mockito.mock(Layout.class);
+
+		Mockito.when(
+			layout.getPlid()
+		).thenReturn(
+			plid
+		);
+
+		SegmentsExperience sourceSegmentsExperience = Mockito.mock(
+			SegmentsExperience.class);
+		SegmentsExperience targetSegmentsExperience = Mockito.mock(
+			SegmentsExperience.class);
+
+		Mockito.when(
+			sourceSegmentsExperience.getSegmentsExperienceId()
+		).thenReturn(
+			sourceSegmentsExperienceId
+		);
+
+		Mockito.when(
+			sourceSegmentsExperience.getSegmentsExperienceKey()
+		).thenReturn(
+			sourceSegmentsExperienceKey
+		);
+
+		Mockito.when(
+			targetSegmentsExperience.getSegmentsExperienceId()
+		).thenReturn(
+			targetSegmentsExperienceId
+		);
+
+		FragmentEntryLink fragmentEntryLink = Mockito.mock(
+			FragmentEntryLink.class);
+
+		Mockito.when(
+			fragmentEntryLink.getCompanyId()
+		).thenReturn(
+			companyId
+		);
+
+		Mockito.when(
+			fragmentEntryLink.getFragmentEntryLinkId()
+		).thenReturn(
+			sourceFragmentEntryLinkId
+		);
+
+		Mockito.when(
+			fragmentEntryLink.getNamespace()
+		).thenReturn(
+			oldNamespace
+		);
+
+		Mockito.when(
+			fragmentEntryLink.getEditableValuesJSONObject()
+		).thenReturn(
+			JSONUtil.put(
+				"instanceId", oldNamespace
+			).put(
+				"portletId", portletId
+			)
+		);
+
+		Mockito.when(
+			fragmentEntryLink.clone()
+		).thenAnswer(
+			inv -> {
+				FragmentEntryLink clone = Mockito.mock(FragmentEntryLink.class);
+
+				Mockito.when(
+					clone.getNamespace()
+				).thenReturn(
+					newNamespace
+				);
+
+				Mockito.when(
+					clone.getEditableValuesJSONObject()
+				).thenReturn(
+					JSONUtil.put(
+						"instanceId", newNamespace
+					).put(
+						"portletId", portletId
+					)
+				);
+
+				return clone;
+			}
+		);
+
+		Mockito.when(
+			portletRegistry.getFragmentEntryLinkPortletIds(fragmentEntryLink)
+		).thenReturn(
+			List.of(sourcePortletId)
+		);
+
+		String rootPortletId = PortletIdCodec.decodePortletName(portletId);
+
+		LayoutStructure layoutStructure = Mockito.mock(LayoutStructure.class);
+		FragmentStyledLayoutStructureItem fragmentStyledLayoutStructureItem =
+			Mockito.mock(FragmentStyledLayoutStructureItem.class);
+
+		Mockito.when(
+			layoutStructure.getLayoutStructureItemByFragmentEntryLinkId(
+				sourceFragmentEntryLinkId)
+		).thenReturn(
+			fragmentStyledLayoutStructureItem
+		);
+
+		try (MockedStatic<FragmentEntryLinkLocalServiceUtil>
+				fragmentEntryLinkMockedStatic = Mockito.mockStatic(
+					FragmentEntryLinkLocalServiceUtil.class);
+			MockedStatic<LayoutStructureUtil> layoutStructureUtilMockedStatic =
+				Mockito.mockStatic(LayoutStructureUtil.class);
+			MockedStatic<LayoutPageTemplateStructureLocalServiceUtil>
+				layoutPageTemplateStructureLocalServiceUtilMockedStatic =
+					Mockito.mockStatic(
+						LayoutPageTemplateStructureLocalServiceUtil.class);
+			MockedStatic<ResourcePermissionLocalServiceUtil>
+				resourcePermissionLocalServiceUtilMockedStatic =
+					Mockito.mockStatic(
+						ResourcePermissionLocalServiceUtil.class);
+			MockedStatic<PortletPermissionUtil>
+				portletPermissionUtilMockedStatic = Mockito.mockStatic(
+					PortletPermissionUtil.class);
+			MockedStatic<CounterLocalServiceUtil>
+				counterLocalServiceUtilMockedStatic = Mockito.mockStatic(
+					CounterLocalServiceUtil.class)) {
+
+			fragmentEntryLinkMockedStatic.when(
+				() ->
+					FragmentEntryLinkLocalServiceUtil.
+						getFragmentEntryLinksBySegmentsExperienceId(
+							groupId, sourceSegmentsExperienceId, plid)
+			).thenReturn(
+				Collections.singletonList(fragmentEntryLink)
+			);
+
+			fragmentEntryLinkMockedStatic.when(
+				() -> FragmentEntryLinkLocalServiceUtil.addFragmentEntryLink(
+					Mockito.any())
+			).thenAnswer(
+				(Answer<FragmentEntryLink>)inv -> inv.getArgument(0)
+			);
+
+			layoutStructureUtilMockedStatic.when(
+				() -> LayoutStructureUtil.getLayoutStructure(
+					groupId, plid, sourceSegmentsExperienceId)
+			).thenReturn(
+				layoutStructure
+			);
+
+			ResourcePermission resourcePermission = Mockito.mock(
+				ResourcePermission.class);
+
+			Mockito.when(
+				resourcePermission.getRoleId()
+			).thenReturn(
+				roleId
+			);
+
+			Mockito.when(
+				resourcePermission.getActionIds()
+			).thenReturn(
+				resourceActionIds
+			);
+
+			LayoutPageTemplateStructure layoutPageTemplateStructure =
+				Mockito.mock(LayoutPageTemplateStructure.class);
+
+			JSONObject structureDataJSONObject = JSONUtil.put("items", "");
+
+			Mockito.when(
+				layoutStructure.toJSONObject()
+			).thenReturn(
+				structureDataJSONObject
+			);
+
+			Mockito.when(
+				layoutPageTemplateStructure.getData(Mockito.anyLong())
+			).thenReturn(
+				structureDataJSONObject.toString()
+			);
+
+			layoutPageTemplateStructureLocalServiceUtilMockedStatic.when(
+				() ->
+					LayoutPageTemplateStructureLocalServiceUtil.
+						fetchLayoutPageTemplateStructure(groupId, plid)
+			).thenReturn(
+				layoutPageTemplateStructure
+			);
+
+			resourcePermissionLocalServiceUtilMockedStatic.when(
+				() -> ResourcePermissionLocalServiceUtil.getResourcePermissions(
+					companyId, rootPortletId,
+					ResourceConstants.SCOPE_INDIVIDUAL, sourcePrimaryKey)
+			).thenReturn(
+				Collections.singletonList(resourcePermission)
+			);
+
+			counterLocalServiceUtilMockedStatic.when(
+				() -> CounterLocalServiceUtil.increment(
+					ResourcePermission.class.getName())
+			).thenReturn(
+				copiedResourcePermissionId
+			);
+
+			resourcePermissionLocalServiceUtilMockedStatic.when(
+				() ->
+					ResourcePermissionLocalServiceUtil.createResourcePermission(
+						copiedResourcePermissionId)
+			).thenReturn(
+				Mockito.mock(ResourcePermission.class)
+			);
+
+			ArgumentCaptor<ResourcePermission> addedPermission =
+				ArgumentCaptor.forClass(ResourcePermission.class);
+
+			resourcePermissionLocalServiceUtilMockedStatic.when(
+				() -> ResourcePermissionLocalServiceUtil.addResourcePermission(
+					addedPermission.capture())
+			).thenReturn(
+				null
+			);
+
+			portletPermissionUtilMockedStatic.when(
+				() -> PortletPermissionUtil.getPrimaryKey(
+					Mockito.anyLong(), Mockito.anyString())
+			).thenAnswer(
+				inv -> {
+					String portletIdArg = inv.getArgument(1);
+
+					if (portletIdArg.equals(sourcePortletId)) {
+						return sourcePrimaryKey;
+					}
+
+					if (portletIdArg.equals(targetPortletId)) {
+						return targetPrimaryKey;
+					}
+
+					return StringPool.BLANK;
+				}
+			);
+
+			SegmentsExperienceUtil.copySegmentsExperienceData(
+				commentManager, groupId, layout, portletRegistry,
+				sourceSegmentsExperience, targetSegmentsExperience,
+				key -> Mockito.mock(ServiceContext.class), userId);
+
+			Assert.assertFalse(
+				addedPermission.getAllValues(
+				).isEmpty());
+
+			ResourcePermission copiedResourcePermission =
+				addedPermission.getValue();
+
+			Mockito.verify(
+				copiedResourcePermission
+			).setCompanyId(
+				companyId
+			);
+
+			Mockito.verify(
+				copiedResourcePermission
+			).setName(
+				rootPortletId
+			);
+
+			Mockito.verify(
+				copiedResourcePermission
+			).setScope(
+				ResourceConstants.SCOPE_INDIVIDUAL
+			);
+
+			Mockito.verify(
+				copiedResourcePermission
+			).setPrimKey(
+				targetPrimaryKey
+			);
+
+			Mockito.verify(
+				copiedResourcePermission
+			).setRoleId(
+				roleId
+			);
+
+			Mockito.verify(
+				copiedResourcePermission
+			).setActionIds(
+				resourceActionIds
+			);
+
+			Mockito.verify(
+				copiedResourcePermission
+			).setViewActionId(
+				resourcePermission.isViewActionId()
+			);
+		}
 	}
 
 	@Test
@@ -89,5 +500,10 @@ public class SegmentsExperienceUtilTest {
 				SegmentsExperienceUtil.getSegmentsExperienceJSONObject(
 					segmentsExperience)));
 	}
+
+	private MockedStatic<PortletPreferencesLocalServiceUtil>
+		_portletPreferencesLocalServiceUtilMockedStatic;
+	private MockedStatic<SegmentsExperimentLocalServiceUtil>
+		_segmentsExperimentLocalServiceUtilMockedStatic;
 
 }
