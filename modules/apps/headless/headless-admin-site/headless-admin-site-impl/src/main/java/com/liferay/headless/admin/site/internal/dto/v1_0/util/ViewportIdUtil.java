e5db723688b072ea70dc6b142f17ff88cdd29b79
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-12-09 21:19:30 +0100

    LPD-69118 Add method to convert from internal to external type

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/util/ViewportIdUtil.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/util/ViewportIdUtil.java
@@ -8,6 +8,8 @@ package com.liferay.headless.admin.site.internal.dto.v1_0.util;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 /**
  * @author Mikel Lorza
@@ -22,6 +24,21 @@ public class ViewportIdUtil {
 		).put(
 			"Tablet", "tablet"
 		).build();
+
+	public static String toExternalType(String internalType) {
+		Set<String> externalTypes = externalToInternalValuesMap.keySet();
+
+		for (String externalType : externalTypes) {
+			if (Objects.equals(
+					internalType,
+					externalToInternalValuesMap.get(externalType))) {
+
+				return externalType;
+			}
+		}
+
+		return null;
+	}
 
 	public static String toInternalValue(String label) {
 		return externalToInternalValuesMap.get(label);
