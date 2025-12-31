7c1017d5ab4383af9680c0101c0e9a0153df66b2
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-12-31 12:37:44 +0100

    LPD-74331 Ensure that in the target site, a layout with the same layout ID but a different externalReferenceCode exists, which is essential to reproduce the issue

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/SitePageResourceTest.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/SitePageResourceTest.java
@@ -2181,6 +2181,20 @@ public class SitePageResourceTest extends BaseSitePageResourceTestCase {
 				layoutIdFriendlyURL, layout.getFriendlyURL(locale));
 		}
 
+		Layout testGroupLayout = LayoutTestUtil.addTypePortletLayout(testGroup);
+
+		while (testGroupLayout.getLayoutId() < layout.getLayoutId()) {
+			testGroupLayout = LayoutTestUtil.addTypePortletLayout(testGroup);
+		}
+
+		testGroupLayout = _layoutLocalService.fetchLayout(
+			testGroup.getGroupId(), false, layout.getLayoutId());
+
+		Assert.assertNotNull(testGroupLayout);
+		Assert.assertNotEquals(
+			layout.getExternalReferenceCode(),
+			testGroupLayout.getExternalReferenceCode());
+
 		sitePageResource.putSiteSitePage(
 			testGroup.getExternalReferenceCode(),
 			layout.getExternalReferenceCode(),
