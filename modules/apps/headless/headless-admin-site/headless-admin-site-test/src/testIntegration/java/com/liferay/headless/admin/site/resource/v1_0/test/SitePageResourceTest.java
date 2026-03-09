208ead5872271e4a05e74bc071495664c9d3a970
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-03-09 16:20:49 +0100

    LPD-81793 Add an integration test to expose the issue

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/SitePageResourceTest.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/SitePageResourceTest.java
@@ -412,7 +412,7 @@ public class SitePageResourceTest extends BaseSitePageResourceTestCase {
 	@TestInfo(
 		{
 			"LPD-72013", "LPD-74331", "LPD-75450", "LPD-77124", "LPD-77505",
-			"LPD-77576", "LPD-77852", "LPD-78667", "LPD-79415"
+			"LPD-77576", "LPD-77852", "LPD-78667", "LPD-79415", "LPD-81793"
 		}
 	)
 	public void testPutSiteSitePage() throws Exception {
@@ -445,6 +445,7 @@ public class SitePageResourceTest extends BaseSitePageResourceTestCase {
 		_testPutSiteSitePageWithPageElements();
 		_testPutSiteSitePageWithPageExperiences();
 		_testPutSiteSitePageWithPageSpecifications();
+		_testPutSiteSitePageWithParentLayout();
 		_testPutSiteSitePageWithPriority();
 		_testPutSiteSitePageWithWidgetPageSettings();
 		_testPutSiteSitePageWithWidgetPageSettingsWithWidgetPageTemplate();
@@ -1244,6 +1245,30 @@ public class SitePageResourceTest extends BaseSitePageResourceTestCase {
 		objectFieldSetting.setValue(value);
 
 		return objectFieldSetting;
+	}
+
+	private SitePage _exportImportSitePage(
+			Layout layout, SitePageResource sitePageResource)
+		throws Exception {
+
+		SitePage sitePage = sitePageResource.getSiteSitePage(
+			irrelevantGroup.getExternalReferenceCode(),
+			layout.getExternalReferenceCode());
+
+		_assertSitePage(layout, sitePage);
+
+		_testPutSiteSitePage(sitePage, testGroup, sitePage);
+
+		Layout importedLayout =
+			_layoutLocalService.getLayoutByExternalReferenceCode(
+				layout.getExternalReferenceCode(), testGroup.getGroupId());
+		SitePage importedSitePage = sitePageResource.getSiteSitePage(
+			testGroup.getExternalReferenceCode(),
+			layout.getExternalReferenceCode());
+
+		_assertSitePage(importedLayout, importedSitePage);
+
+		return importedSitePage;
 	}
 
 	private CustomMetaTag[] _getCustomMetaTags() {
@@ -3782,6 +3807,41 @@ public class SitePageResourceTest extends BaseSitePageResourceTestCase {
 		PageSpecificationsTestUtil.assertWidgetPageSpecifications(
 			sitePage.getPageSpecifications(),
 			putSitePage.getPageSpecifications());
+	}
+
+	private void _testPutSiteSitePageWithParentLayout() throws Exception {
+		Layout parentLayout = LayoutTestUtil.addTypePortletLayout(
+			irrelevantGroup);
+
+		SitePageResource sitePageResource = _getSitePageResource(
+			"pageSpecifications");
+
+		SitePage importedParentSitePage = _exportImportSitePage(
+			parentLayout, sitePageResource);
+
+		Assert.assertTrue(
+			Validator.isNull(
+				importedParentSitePage.
+					getParentSitePageExternalReferenceCode()));
+
+		Layout layout = LayoutTestUtil.addTypePortletLayout(
+			irrelevantGroup, parentLayout.getPlid());
+
+		SitePage importedSitePage = _exportImportSitePage(
+			layout, sitePageResource);
+
+		Assert.assertEquals(
+			parentLayout.getExternalReferenceCode(),
+			importedSitePage.getParentSitePageExternalReferenceCode());
+
+		layout = _layoutLocalService.updateParentLayoutId(
+			layout.getPlid(), LayoutConstants.DEFAULT_PARENT_LAYOUT_ID);
+
+		importedSitePage = _exportImportSitePage(layout, sitePageResource);
+
+		Assert.assertTrue(
+			Validator.isNull(
+				importedSitePage.getParentSitePageExternalReferenceCode()));
 	}
 
 	private void _testPutSiteSitePageWithPriority() throws Exception {
