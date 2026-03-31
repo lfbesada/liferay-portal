4b2c855f0035bc7a10f22db73c0927cf4838a695
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-03-31 13:11:01 +0200

    LPD-82607 Pass an empty JSONObject instead of set null editable values

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/util/FragmentEntryLinkUtil.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/util/FragmentEntryLinkUtil.java
@@ -10,6 +10,7 @@ import com.liferay.fragment.model.FragmentEntryLink;
 import com.liferay.fragment.processor.DefaultFragmentEntryProcessorContext;
 import com.liferay.fragment.processor.FragmentEntryProcessorRegistry;
 import com.liferay.layout.util.LayoutServiceContextHelperUtil;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
@@ -68,12 +69,10 @@ public class FragmentEntryLinkUtil {
 
 			String editableValues = fragmentEntryLink.getEditableValues();
 
-			fragmentEntryLink.setEditableValues(null);
-
 			try {
 				return fragmentEntryProcessorRegistry.
 					processFragmentEntryLinkHTML(
-						fragmentEntryLink,
+						JSONFactoryUtil.createJSONObject(), fragmentEntryLink,
 						new DefaultFragmentEntryProcessorContext(
 							fragmentEntryLink.getCompanyId(),
 							httpServletRequest, httpServletResponse,
