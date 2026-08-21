f7c50cbe7d5cdb388b5c5bfeb0af942c791c4bb4
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-08-21 09:30:49 +0200

    LPD-103339 Populate the previews in the PageSpecificationVersionDTOConverter

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/converter/PageSpecificationVersionDTOConverter.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/converter/PageSpecificationVersionDTOConverter.java
@@ -5,15 +5,28 @@
 
 package com.liferay.headless.admin.site.internal.dto.v1_0.converter;
 
+import com.liferay.headless.admin.site.dto.v1_0.ContentPageSpecification;
 import com.liferay.headless.admin.site.dto.v1_0.PageSpecificationVersion;
+import com.liferay.headless.admin.site.dto.v1_0.PageSpecificationVersionPageExperience;
 import com.liferay.headless.admin.site.internal.dto.v1_0.util.CreatorUtil;
 import com.liferay.layout.content.model.LayoutContentVersion;
+import com.liferay.layout.content.model.LayoutContentVersionPreview;
+import com.liferay.layout.content.service.LayoutContentVersionPreviewLocalService;
+import com.liferay.petra.function.transform.TransformUtil;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Lourdes Fernández Besada
@@ -53,6 +66,9 @@ public class PageSpecificationVersionDTOConverter
 				setName(
 					() -> layoutContentVersion.getName(
 						LocaleUtil.getSiteDefault()));
+				setPageSpecificationVersionPageExperiences(
+					() -> _toPageSpecificationVersionPageExperiences(
+						layoutContentVersion));
 				setStatus(
 					() -> {
 						if (layoutContentVersion.getStatus() ==
@@ -68,5 +84,68 @@ public class PageSpecificationVersionDTOConverter
 			}
 		};
 	}
+
+	private PageSpecificationVersionPageExperience[]
+			_toPageSpecificationVersionPageExperiences(
+				LayoutContentVersion layoutContentVersion)
+		throws PortalException {
+
+		ContentPageSpecification contentPageSpecification =
+			ContentPageSpecification.toDTO(layoutContentVersion.getData());
+
+		if ((contentPageSpecification == null) ||
+			(contentPageSpecification.getPageExperiences() == null)) {
+
+			return new PageSpecificationVersionPageExperience[0];
+		}
+
+		Map<String, List<String>> languageIdsBySegmentsExperienceERC =
+			new HashMap<>();
+
+		for (LayoutContentVersionPreview layoutContentVersionPreview :
+				_layoutContentVersionPreviewLocalService.
+					getLayoutContentVersionPreviews(
+						layoutContentVersion.getLayoutContentVersionId())) {
+
+			List<String> languageIds =
+				languageIdsBySegmentsExperienceERC.computeIfAbsent(
+					layoutContentVersionPreview.getSegmentsExperienceERC(),
+					key -> new ArrayList<>());
+
+			languageIds.add(layoutContentVersionPreview.getLanguageId());
+		}
+
+		return TransformUtil.transform(
+			contentPageSpecification.getPageExperiences(),
+			pageExperience -> {
+				PageSpecificationVersionPageExperience
+					pageSpecificationVersionPageExperience =
+						new PageSpecificationVersionPageExperience();
+
+				pageSpecificationVersionPageExperience.
+					setAvailablePreviewLanguageIds(
+						() -> {
+							List<String> languageIds =
+								languageIdsBySegmentsExperienceERC.getOrDefault(
+									pageExperience.getExternalReferenceCode(),
+									Collections.emptyList());
+
+							return languageIds.toArray(new String[0]);
+						});
+				pageSpecificationVersionPageExperience.setExternalReferenceCode(
+					pageExperience::getExternalReferenceCode);
+				pageSpecificationVersionPageExperience.setName_i18n(
+					pageExperience::getName_i18n);
+				pageSpecificationVersionPageExperience.setPriority(
+					pageExperience::getPriority);
+
+				return pageSpecificationVersionPageExperience;
+			},
+			PageSpecificationVersionPageExperience.class);
+	}
+
+	@Reference
+	private LayoutContentVersionPreviewLocalService
+		_layoutContentVersionPreviewLocalService;
 
 }
